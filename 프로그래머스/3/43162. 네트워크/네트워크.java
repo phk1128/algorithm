0b563e8f238f7edb6f714506679988870948e3ca
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-01-17 17:45:46 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,23 +1,25 @@
 class Solution {
     private boolean[] visited;
-    private int answer = 0;
+    private int[][] computers;
     public int solution(int n, int[][] computers) {
         visited = new boolean[n];
+        this.computers = computers;
+        int answer = 0;
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(computers,i,n);
+                dfs(i,n);
                 answer++;
             }
         }
         return answer;
     }
     
-    public void dfs(int[][] computers,int node, int n) {
+    public void dfs(int node, int n) {
         for (int i = 0; i < n; i++) {
             if (!visited[i] && computers[node][i] == 1) {
                 visited[i] = true;
-                dfs(computers,i, n);
+                dfs(i, n);
             }
         }
     }
