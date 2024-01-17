0d7fcb94b09cd03073ba9bdfebfbd2bf58e37443
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-01-17 20:21:24 +0900

    [level 2] Title: 주차 요금 계산, Time: 7.75 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,70 +2,124 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
+    private final RecordGroup recordGroup = new RecordGroup();
+    
     public int[] solution(int[] fees, String[] records) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String,List<String>> recordsMap= new TreeMap<>();
-        
-        for(String record : records) {
-            String[] recordList = record.split(" ");
-            List<String> value = recordsMap.get(recordList[1]);
-            if(value == null) {value = new ArrayList<>();}
-            value.add(recordList[0]);
-            recordsMap.put(recordList[1], value);
-        }
-        
-        for(String carNum : recordsMap.keySet()) {
-            List<String> times = recordsMap.get(carNum);
-            if(times.size()%2 != 0) {times.add("23:59");}
-            recordsMap.put(carNum, times);
-        }
-        
-        for(String carNum : recordsMap.keySet()) {
-            List<String> times = recordsMap.get(carNum);
-            List<Integer> convertResult = convertToMinutes(times);
-            System.out.println(convertResult); 
-            int tax = calculateTax(convertResult, fees);
-            answer.add(tax);
-        }
-        
-        
-        return answer.stream().mapToInt(i->i).toArray();
-    
-    }
-    
-    
-    public List<Integer> convertToMinutes(List<String> times) {
+        int[] answer = {};
         List<Integer> result = new ArrayList<>();
         
-        for(String time : times) {
-            String[] timeList = time.split(":");
-            int hours = Integer.parseInt(timeList[0])*60;
-            int minutes = Integer.parseInt(timeList[1]);
-            result.add(hours+minutes);
-        }
+        for (String record : records) {
+            String[] splitRecord = record.split(" ");
+            String time = splitRecord[0];
+            String carNumber = splitRecord[1];
             
-        return result;
+            if (!recordGroup.isExistByCarNumber(carNumber)) {
+                recordGroup.addRecord(new Record(carNumber));
+            }
+            
+            Record rec = recordGroup.findByCarNumber(carNumber);
+            int convertedTime = (Integer.parseInt(time.split(":")[0]) * 60) + (Integer.parseInt(time.split(":")[1]));
+            rec.addTime(convertedTime);
+        }
+        
+        
+        for (Record record : recordGroup.getRecords()) {
+            int price = 0;
+            double usedTime = record.getUsedTime();
+            if (usedTime > fees[0]) {
+                price += Math.ceil((usedTime - fees[0]) / fees[2]) * fees[3];
+            }
+            price += fees[1];
+            result.add(price);
+        }
+        
+        answer = result.stream().mapToInt(i->i).toArray();
+        
+        return answer;
     }
     
-    public int calculateTax(List<Integer> times, int[] fees) {
-        double usedTime = 0;
+    static class RecordGroup {
+        private final List<Record> records;
         
-        while(!times.isEmpty()) {
-            double inTime = times.remove(0);
-            double outTime = times.remove(0);
+        public RecordGroup() {
+            this.records = new ArrayList<Record>();
+        }
+        
+        public void addRecord(Record record) {
+            records.add(record);
+        }
+        
+        public Record findByCarNumber(String carNumber) {
+            return this.records.stream()
+                .filter(record -> Objects.equals(carNumber, record.getCarNumber()))
+                .findAny()
+                .orElse(null);
+        }
+        
+        public List<Record> getRecords() {
+            return this.records.stream().sorted(Comparator.comparing(Record::getCarNumber)).collect(Collectors.toList());
+        }
+        
+        private boolean isExistByCarNumber(String carNumber) {
+            return this.records.stream()
+                .anyMatch(record -> Objects.equals(carNumber, record.getCarNumber()));
+        }
+    }
+    
+    static class Record {
+        private String carNumber; 
+        private final List<Integer> times;
+        
+        public Record(String carNumber) {
+            this.carNumber = carNumber;
+            this.times = new ArrayList<>();
+        }
+        
+        public String getCarNumber() {
+            return this.carNumber;
+        }
+        
+        public void addTime(int time) {
+            this.times.add(time);
+        }
+        
+        public double getUsedTime() {
+            if (times.size() % 2 != 0) {
+                times.add(1439); // 23:59를 분단위로 환산
+            }
             
-            usedTime += (outTime - inTime);
+            return calculateTime(0,0);
+            
         }
-        double tmp = 0;
-        if (usedTime > fees[0]) {
-            tmp = (usedTime-fees[0])/fees[2];
+        private double calculateTime(double time, int depth) {
+            if (depth == times.size()) {
+                return time;
+            }
+            if (depth % 2 == 0) {
+                time -= times.get(depth);
+            } else {
+                time += times.get(depth);
+            }
+            
+            return calculateTime(time, depth+1);
         }
         
-        int used = (int) Math.ceil(tmp);
-        int tax = fees[1] + used*fees[3];
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Record)) {
+                return false;
+            }
+            Record record = (Record) o;
+            return Objects.equals(this.carNumber, record.carNumber);
+        }
         
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.carNumber);
+        }
         
-        return tax;
     }
 }
-    
