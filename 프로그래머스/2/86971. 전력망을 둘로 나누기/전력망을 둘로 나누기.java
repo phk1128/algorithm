ab07f00fe832749665968bdd4326f2b5a77275cc
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-01-19 16:12:14 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.87 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,52 +1,65 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    private List<List<Integer>> nodes = new ArrayList<>();
+    private List<List<Integer>> graph;
     private boolean[] visited;
     
     public int solution(int n, int[][] wires) {
-        int min = Integer.MAX_VALUE;
         int answer = -1;
+        int min = Integer.MAX_VALUE;
+        graph = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
-            nodes.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
         
         for (int[] wire : wires) {
             int start = wire[0];
             int end = wire[1];
             
-            nodes.get(start).add(end);
-            nodes.get(end).add(start);
+            graph.get(start).add(end);
+            graph.get(end).add(start);
         }
         
         for (int[] wire : wires) {
             int start = wire[0];
             int end = wire[1];
-            
-            nodes.get(start).remove(Integer.valueOf(end));
-            nodes.get(end).remove(Integer.valueOf(start));
             
             visited = new boolean[n+1];
-            int result = dfs(1);
+            
+            graph.get(start).remove(Integer.valueOf(end));
+            graph.get(end).remove(Integer.valueOf(start));
+            
+            int result = bfs();
             
             min = Math.min(min, Math.abs(result - (n - result)));
             
-            nodes.get(start).add(end);
-            nodes.get(end).add(start);
+            graph.get(start).add(end);
+            graph.get(end).add(start);
             
         }
         answer = min;
+        
         return answer;
     }
     
-    public int dfs(int start) {
+    private int bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visited[1] = true;
         int count = 1;
-        visited[start] = true;
-        for (int n : nodes.get(start)) {
-            if (!visited[n]) {
-                count += dfs(n);
+        
+        while(!queue.isEmpty()) {
+            int start = queue.poll();
+            for (int node : graph.get(start)) {
+                if (!visited[node]) {
+                    visited[node] = true;
+                    queue.add(node);
+                    count++;
+                }
             }
         }
         return count;
+        
     }
 }
