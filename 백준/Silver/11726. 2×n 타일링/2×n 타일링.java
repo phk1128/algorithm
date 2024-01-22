890a966a4ce02d1d3edf94c7ddd5f88a86d2da01
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-01-22 21:19:31 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -18,19 +18,12 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         result = new ArrayList<>(List.of(0L,1L,2L));
-        calculate(3, n);
-        bw.close();
-    }
-
-    private static void calculate(int idx, int n) throws IOException{
-        if (idx == n + 1 || n < 3) {
-            bw.write(String.valueOf(result.get(n)));
-            bw.flush();
-            return;
+        for (int i = 3; i < n + 1; i++) {
+            result.add((result.get(i-1) + result.get(i-2)) % 10_007);
         }
-        long a = result.get(idx - 2);
-        long b = result.get(idx - 1);
-        result.add((a+b)  % 10_007);
-        calculate(idx + 1, n);
+
+        bw.write(String.valueOf(result.get(n)));
+        bw.flush();
+        bw.close();
     }
 }
