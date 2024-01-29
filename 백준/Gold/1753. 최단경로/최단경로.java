422ab04f59ff72bf943ec1e88ebe65e5f98105d2
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-01-29 22:58:07 +0900

    [Gold IV] Title: 최단경로, Time: 1088 ms, Memory: 110196 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
     private static BufferedReader br;
     private static BufferedWriter bw;
     private static StringTokenizer st;
@@ -10,6 +11,7 @@ public class Main {
     private static boolean[] visited;
 
     public static void main(String[] args) throws IOException{
+
         br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -19,30 +21,30 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
-        cost = new int[v+1];
-        Arrays.fill(cost, Integer.MAX_VALUE);
-        visited = new boolean[v+1];
 
-
+        cost = new int[v + 1];
         graph = new ArrayList[v + 1];
+        visited = new boolean[v + 1];
 
         for (int i = 1; i <= v; i++) {
             graph[i] = new ArrayList<>();
         }
+        Arrays.fill(cost, Integer.MAX_VALUE);
 
         for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-            graph[start].add(new Edge(end,weight));
+
+            graph[start].add(new Edge(end, weight));
         }
 
         dijkstra(k);
 
         for (int i = 1; i <= v; i++) {
             if (cost[i] == Integer.MAX_VALUE) {
-                bw.write("INF"+ "\n");
+                bw.write("INF" + "\n");
                 bw.flush();
                 continue;
             }
@@ -54,40 +56,45 @@ public class Main {
     }
 
     private static void dijkstra(int k) {
+
+        cost[k] = 0;
         PriorityQueue<Edge> queue = new PriorityQueue<>();
         queue.offer(new Edge(k,0));
-        cost[k] = 0;
-        while (!queue.isEmpty()) {
+
+        while(!queue.isEmpty()) {
             Edge edge = queue.poll();
-            int node = edge.getNode();
-            if (visited[node]) {
+            int end = edge.getEnd();
+
+            if (visited[end]) {
                 continue;
             }
-            visited[node] = true;
-            for (Edge e : graph[node]) {
-                if (cost[e.getNode()] > cost[node] + e.getWeight()) {
-                    cost[e.getNode()] = cost[node] + e.getWeight();
-                    queue.offer(new Edge(e.getNode(), cost[node] + e.getWeight()));
+            visited[end] = true;
+
+            for (Edge e : graph[end]) {
+                if (cost[e.getEnd()] > cost[end] + e.getWeight()) {
+                    cost[e.getEnd()] = cost[end] + e.getWeight();
+                    queue.offer(new Edge(e.getEnd(), cost[end] + e.getWeight()));
                 }
             }
         }
     }
 
-    static class Edge implements Comparable<Edge>{
-        private int node;
+    static class Edge implements Comparable<Edge> {
+
+        private int end;
         private int weight;
 
-        public Edge(int node, int weight) {
-            this.node = node;
+        public Edge(int end, int weight) {
+            this.end = end;
             this.weight = weight;
         }
 
-        private int getNode() {
-            return this.node;
+        public int getEnd() {
+            return end;
         }
 
-        private int getWeight() {
-            return this.weight;
+        public int getWeight() {
+            return weight;
         }
 
         @Override
