ecf8cde151d35108d4fc2e52330bf79f5438aa39
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-01-30 17:41:53 +0900

    [level 3] Title: 아이템 줍기, Time: 0.79 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -7,7 +7,7 @@ class Solution {
     private int itemX;
     private int itemY;
     private int[][] directions;
-
+    
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         answer = Integer.MAX_VALUE;
         this.rectangle = rectangle;
@@ -22,20 +22,23 @@ class Solution {
     }
 
     private void dfs(int x, int y, int rec, int count) {
+        
         if (itemX == x && itemY == y) {
             answer = Math.min(answer, count / 2);
             return;
         }
+        
+        // 사각형 내부에 포함되어 있는 좌표면 리턴
         try {
             rec = findRectangle(x, y, rec);
         } catch(IllegalArgumentException e) {
             return;
         }
-
+        
+        // 테두리 밖쪽 좌표면 리턴
         if (rectangle[rec][0] * 2 > x || rectangle[rec][2] * 2 < x || rectangle[rec][1] * 2 > y || rectangle[rec][3] * 2 < y) {
             return;
         }
-
 
         visited[x][y] = true;
 
@@ -50,6 +53,7 @@ class Solution {
     }
 
     private int findRectangle(int x, int y, int rec) {
+        // 사각형을 전부 돌면서 좌표가 사각형 내부에 있는 좌표인지 체크 + 사각형이 바뀌었다면 바뀐 사각형으로 rec변경
         for (int i = 0; i < rectangle.length; i++) {
             if (rectangle[i][0] * 2 <= x && rectangle[i][2] * 2 >= x && rectangle[i][1] * 2 <= y && rectangle[i][3] * 2 >= y) {
                 if (rectangle[i][0] * 2 < x && rectangle[i][2] * 2 > x && rectangle[i][1] * 2 < y && rectangle[i][3] * 2 > y) {
