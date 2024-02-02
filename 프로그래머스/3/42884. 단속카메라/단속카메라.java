b5d4f2b18ee8d3ef0c8173acf801b76c40d22708
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-02-02 15:11:40 +0900

    [level 3] Title: 단속카메라, Time: 22.38 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -11,18 +11,17 @@ class Solution {
         }
         Collections.sort(routeList, (route1, route2) -> (route1[1] - route2[1]));
         
-        while(!routeList.isEmpty()) {
-            int r = routeList.get(0)[1];
-            
-            for (int i = 0; i < routeList.size(); i++) {
-                if (r >= routeList.get(i)[0]) {
-                    routeList.remove(i);
-                    i--;
-                }
-            }
-            answer++;
-        }
+        int r = routeList.get(0)[1];
+        answer++;
         
+        while(!routeList.isEmpty()) {
+            if (r >= routeList.get(0)[0]) {
+                routeList.remove(0);
+            } else {
+                r = routeList.get(0)[1];
+                answer++;
+            }
+        }
         return answer;
     }
 }
