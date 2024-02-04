b17ba3eb455cb3d305b0427260516a07667f7847
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-02-04 23:09:21 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 140 ms, Memory: 14932 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,71 +1,100 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+    // input
     private static BufferedReader br;
-    private static BufferedWriter bw;
-    private static StringTokenizer st;
-    private static long[][] kevin;
-    private static int min;
-    private static int answer;
+
+    // variables
+    private static final int INF = Integer.MAX_VALUE / 4;
+    private static int N, M, ans, min;
+    private static List<List<Node>> adjList;
+
+    private static class Node implements Comparable<Node> {
+        int node;
+        int dist;
+
+        public Node(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        } // End of Node()
+
+        @Override
+        public int compareTo(Node o) {
+            return dist = o.dist;
+        } // End of compareTo()
+    } // End of Node class
 
     public static void main(String[] args) throws IOException {
-
         br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        input();
 
-        kevin = new long[n + 1][n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(kevin[i], Integer.MAX_VALUE);
-            kevin[i][i] = 0;
-            kevin[i][0] = 0;
-        }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            kevin[start][end] = 1;
-            kevin[end][start] = 1;
-        }
-
-        floyd(n);
-
-        min = Integer.MAX_VALUE;
-        answer = n;
-
-        for (int i = 1; i <= n; i++) {
-            int sum = (int) Arrays.stream(kevin[i]).sum();
-            min = Math.min(min, sum);
-        }
-
-        for (int i = 1; i <= n; i++) {
-            int sum = (int) Arrays.stream(kevin[i]).sum();
-            if (sum == min) {
-                answer = Math.min(answer, i);
-            }
-        }
-
-        bw.write(String.valueOf(answer));
-        bw.flush();
+        bw.write(solve());
         bw.close();
-    }
+    } // End of main()
 
-    private static void floyd(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    if (kevin[j][k] >= kevin[j][i] + kevin[i][k]) {
-                        kevin[j][k] = kevin[j][i] + kevin[i][k];
-                    }
+    private static String solve() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            dijkstra(i);
+        }
+
+        sb.append(ans);
+        return sb.toString();
+    } // End of solve()
+
+    private static void dijkstra(int start) {
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        PriorityQueue<Node> pque = new PriorityQueue<>();
+        pque.offer(new Node(start, 0));
+
+        while (!pque.isEmpty()) {
+            Node now = pque.poll();
+
+            for (Node next : adjList.get(now.node)) {
+                if (dist[next.node] > dist[now.node] + next.dist) {
+                    dist[next.node] = dist[now.node] + next.dist;
+                    pque.offer(new Node(next.node, dist[next.node]));
                 }
             }
         }
-    }
-}
+
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += dist[i];
+        }
+
+        if (min > sum) {
+            min = sum;
+            ans = start;
+        }
+    } // End of dijkstra()
+
+    private static void input() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        min = INF;
+
+        adjList = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adjList.get(a).add(new Node(b, 1));
+            adjList.get(b).add(new Node(a, 1));
+        }
+    } // End of input()
+} // End of Main class
