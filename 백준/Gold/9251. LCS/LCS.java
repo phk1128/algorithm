06503d280646c1a54ffc3a163e4c2dee98e31481
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-02-05 17:59:19 +0900

    [Gold V] Title: LCS, Time: 228 ms, Memory: 21220 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,50 +1,52 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.util.*;
+import java.io.*;
+
 public class Main {
- 
-	static char[] str1;
-	static char[] str2;
- 
-	static Integer[][] dp;
-	
-	public static void main(String[] args) throws IOException {
-    
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		str1 = br.readLine().toCharArray();
-		str2 = br.readLine().toCharArray();
- 
-		
-		dp = new Integer[str1.length][str2.length];
-		
-		System.out.println(LCS(str1.length - 1, str2.length - 1));
-		
-	}
-	
-	static int LCS(int x, int y) {
-		
-		// 인덱스 밖 (공집합)일 경우 0 반환
-		if(x == -1 || y == -1) {
-			return 0;
-		}
- 
-		// 만약 탐색하지 않은 인덱스라면?
-		if(dp[x][y] == null) {
-			dp[x][y] = 0;
- 
-			// str1의 x번째 문자와 str2의 y번째 문자가 같은지 검사
-			if(str1[x] == str2[y]) {
-				dp[x][y] = LCS(x - 1, y - 1) + 1;
-			}
- 
-			// 같지 않다면 LCS(dp)[x-1][y]와 LCS(dp)[x,y-1] 중 큰 값으로 초기화
-			else {
-				dp[x][y] = Math.max(LCS(x - 1, y), LCS(x, y - 1));
-			}
-		}
-		
-		return dp[x][y];
-	}
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+    private static String[] sequence;
+    private static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sequence = new String[2];
+        st = new StringTokenizer(br.readLine());
+        sequence[0] = st.nextToken();
+        st = new StringTokenizer(br.readLine());
+        sequence[1] = st.nextToken();
+
+        dp = new int[sequence[1].length()][sequence[0].length()];
+
+        for (int i = 0; i < sequence[1].length(); i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        bw.write(String.valueOf(lcs(sequence[1].length() - 1, sequence[0].length() - 1)));
+        bw.flush();
+        bw.close();
+    }
+
+    private static int lcs(int r, int c) {
+
+        if (r < 0 || c < 0) {
+            return 0;
+        }
+
+        if (dp[r][c] == -1) {
+            dp[r][c] = 0;
+            // sequence[0] = ACAYKP 열 / squence[1] = CAPCAK 행
+            if (Objects.equals(sequence[0].charAt(c), sequence[1].charAt(r))) {
+                dp[r][c] = lcs(r - 1, c - 1) + 1;
+            } else {
+
+                dp[r][c] = Math.max(lcs(r - 1, c), lcs(r, c - 1));
+            }
+
+        }
+        return dp[r][c];
+    }
 }
