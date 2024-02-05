c7af0eccd99f4d6f68ca1584184c9e3273a5b6b7
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-02-05 20:12:28 +0900

    [Gold V] Title: LCS, Time: 176 ms, Memory: 19784 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -2,51 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     private static BufferedReader br;
     private static BufferedWriter bw;
     private static StringTokenizer st;
-    private static String[] sequence;
+    private static String sequence1;
+    private static String sequence2;
     private static int[][] dp;
 
     public static void main(String[] args) throws IOException {
 
         br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        sequence = new String[2];
-        st = new StringTokenizer(br.readLine());
-        sequence[0] = st.nextToken();
-        st = new StringTokenizer(br.readLine());
-        sequence[1] = st.nextToken();
 
-        dp = new int[sequence[1].length()][sequence[0].length()];
+        st = new StringTokenizer(br.readLine());
+        sequence1 = st.nextToken();
+        st = new StringTokenizer(br.readLine());
+        sequence2 = st.nextToken();
 
-        for (int i = 0; i < sequence[1].length(); i++) {
-            Arrays.fill(dp[i], -1);
+        dp = new int[sequence1.length() + 1][sequence2.length() + 1];
+
+        for (int i = 1; i <= sequence1.length(); i++) {
+            for (int j = 1; j <= sequence2.length(); j++) {
+                if (Objects.equals(sequence1.charAt(i - 1), sequence2.charAt(j - 1))) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
 
-        bw.write(String.valueOf(lcs(sequence[1].length() - 1, sequence[0].length() - 1)));
+        bw.write(String.valueOf(dp[sequence1.length()][sequence2.length()]));
         bw.flush();
         bw.close();
-    }
-
-    private static int lcs(int r, int c) {
-
-        if (r < 0 || c < 0) {
-            return 0;
-        }
-
-        if (dp[r][c] == -1) {
-            dp[r][c] = 0;
-            // sequence[0] = ACAYKP 열 / squence[1] = CAPCAK 행
-            if (Objects.equals(sequence[0].charAt(c), sequence[1].charAt(r))) {
-                dp[r][c] = lcs(r - 1, c - 1) + 1;
-            } else {
-
-                dp[r][c] = Math.max(lcs(r - 1, c), lcs(r, c - 1));
-            }
-
-        }
-        return dp[r][c];
     }
 }
