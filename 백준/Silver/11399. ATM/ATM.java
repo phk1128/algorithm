36e4f9a196a45e3ec4010d5617e52ef9783bd999
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-02-13 20:34:59 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14488 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+    private static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        numbers = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+        int sum = 0;
+        int answer = 0;
+
+        for (int number : numbers) {
+            sum += number;
+            answer += sum;
+        }
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+    }
+}
