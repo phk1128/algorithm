c82495f39aae294bd89a4e8096d0118f27b1b4b0
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-02-20 00:40:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 212 ms, Memory: 53856 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+    private static int[][] items;
+
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        items = new int[n + 1][2];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            items[i][0] = w;
+            items[i][1] = v;
+        }
+
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j - items[i][0] >= 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - items[i][0]] + items[i][1]);
+                }
+            }
+        }
+
+        bw.write(String.valueOf(dp[n][k]));
+        bw.flush();
+        bw.close();
+    }
+}
