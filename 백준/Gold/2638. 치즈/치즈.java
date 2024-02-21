c7118bfdbe04b87795b238a46bf2eecd7110c3be
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-02-21 20:12:12 +0900

    [Gold III] Title: 치즈, Time: 276 ms, Memory: 45548 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.util.Map.Entry;
 
 public class Main {
 
@@ -10,10 +9,9 @@ public class Main {
     private static int n;
     private static int m;
     private static int[][] mapView;
+    private static int[][] removeMapview;
     private static boolean[][] visited;
     private static int[][] directions;
-    private static Map<Cheese, Integer> cheeseGroup;
-    private static int removeCount;
     private static int cheeseCount;
 
     public static void main(String[] args) throws IOException {
@@ -27,9 +25,7 @@ public class Main {
 
         mapView = new int[n][m];
         directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        cheeseGroup = new HashMap<>();
         cheeseCount = 0;
-        removeCount = 0;
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -37,17 +33,14 @@ public class Main {
                 mapView[i][j] = Integer.parseInt(st.nextToken());
                 if (mapView[i][j] == 1) {
                     cheeseCount++;
-                    cheeseGroup.put(new Cheese(j, i), 0);
                 }
             }
         }
         int day = 0;
 
-        while (cheeseCount != removeCount) {
-            for (Map.Entry<Cheese, Integer> entry : cheeseGroup.entrySet()) {
-                cheeseGroup.replace(entry.getKey(), 0);
-            }
+        while (cheeseCount != 0) {
             visited = new boolean[n][m];
+            removeMapview = new int[n][m];
             bfsSolve();
             day++;
         }
@@ -71,72 +64,21 @@ public class Main {
                 int newY = y + direction[1];
 
                 if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
+                    if (mapView[newY][newX] == 1) {
+                        if (removeMapview[newY][newX] >= 1) {
+                            mapView[newY][newX] = 0;
+                            visited[newY][newX] = true;
+                            cheeseCount--;
+                        } else {
+                            removeMapview[newY][newX]++;
+                        }
+                    }
                     if (!visited[newY][newX] && mapView[newY][newX] == 0) {
                         visited[newY][newX] = true;
-                        findCheese(newX, newY, queue);
+                        queue.offer(new int[]{newX, newY});
                     }
                 }
             }
-        }
-    }
-
-    private static void findCheese(int x, int y, Queue<int[]> queue) {
-
-        for (int[] direction : directions) {
-            int newX = x + direction[0];
-            int newY = y + direction[1];
-            if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
-                if (mapView[newY][newX] == 1) {
-                    Cheese cheese = new Cheese(newX, newY);
-                    int count = cheeseGroup.get(cheese);
-                    if (count >= 1) {
-                        mapView[newY][newX] = 0;
-                        visited[newY][newX] = true;
-                        cheeseGroup.remove(cheese);
-                        removeCount++;
-                    } else {
-                        cheeseGroup.replace(cheese, cheeseGroup.get(cheese) + 1);
-                    }
-                }
-            }
-            queue.offer(new int[]{x, y});
-        }
-    }
-
-    static class Cheese {
-
-        private int x;
-        private int y;
-
-        public Cheese(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof Cheese)) {
-                return false;
-            }
-            Cheese cheese = (Cheese) o;
-            return Objects.equals(this.x, cheese.x) &&
-                    Objects.equals(this.y, cheese.y);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(this.x, this.y);
         }
     }
 }
