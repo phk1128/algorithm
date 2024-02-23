7ebf523ac04240a04c23603d97a05b7501ad332b
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-02-23 20:54:36 +0900

    [Gold IV] Title: 트리의 지름, Time: 228 ms, Memory: 23112 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -7,7 +7,7 @@ public class Main {
     private static BufferedWriter bw;
     private static StringTokenizer st;
     private static List<Edge>[] graph;
-    private static List<Integer> child;
+    private static int maxIdx;
     private static int max;
     private static boolean[] visited;
 
@@ -20,10 +20,8 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
 
         graph = new ArrayList[n + 1];
-        child = new ArrayList<>();
-        max = 0;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
 
@@ -37,16 +35,13 @@ public class Main {
             graph[end].add(new Edge(start, dist));
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (graph[i].size() == 1) {
-                child.add(i);
-            }
-        }
-
-        for (int c : child) {
-            visited = new boolean[n + 1];
-            recursiveSolve(c, 0);
-        }
+        maxIdx = 0;
+        max = 0;
+        visited = new boolean[n + 1];
+        recursiveSolve(1, 0);
+        visited = new boolean[n + 1];
+        max = 0;
+        recursiveSolve(maxIdx, 0);
 
         bw.write(String.valueOf(max));
         bw.flush();
@@ -58,15 +53,16 @@ public class Main {
 
         visited[start] = true;
 
+        if (sum > max) {
+            max = sum;
+            maxIdx = start;
+        }
+
         for (Edge edge : graph[start]) {
             int node = edge.getNode();
             if (!visited[node]) {
                 recursiveSolve(node, sum + edge.getDist());
             }
-        }
-
-        if (graph[start].size() == 1) {
-            max = Math.max(max, sum);
         }
     }
 
