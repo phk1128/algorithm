df1850bf00854c2271fb87bd91e516bf5fbf1f80
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-03-21 15:18:15 +0900

    [Gold III] Title: 연구소 3, Time: 260 ms, Memory: 29228 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -6,14 +6,14 @@ public class Main {
     private static BufferedReader br;
     private static BufferedWriter bw;
     private static StringTokenizer st;
-    private static int[][] mapView;
     private static int N;
     private static int M;
-    private static List<Integer> totalVirus;
-    private static int[] combi;
-    private static int[][] directions;
-    private static boolean[][] visited;
     private static int emptyZone;
+    private static int[][] mapView;
+    private static List<Virus> virusGroup;
+    private static Virus[] combi;
+    private static boolean[][] visited;
+    private static int[][] directions;
     private static int min;
 
     public static void main(String[] args) throws IOException {
@@ -24,26 +24,29 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         mapView = new int[N][N];
-        totalVirus = new ArrayList<>();
-        directions = new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+        combi = new Virus[M];
+        virusGroup = new ArrayList<>();
         emptyZone = 0;
+        directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        min = Integer.MAX_VALUE;
 
         for (int r = 0; r < N; r++) {
             st = new StringTokenizer(br.readLine());
             for (int c = 0; c < N; c++) {
                 int number = Integer.parseInt(st.nextToken());
-                if (number == 2) {
-                    totalVirus.add((r * N) + c);
-                }
+                mapView[r][c] = number;
+
                 if (number == 0) {
                     emptyZone++;
                 }
-                mapView[r][c] = number;
+
+                if (number == 2) {
+                    virusGroup.add(new Virus(r, c, 0));
+                }
             }
         }
-        combi = new int[M];
-        min = Integer.MAX_VALUE;
 
         if (emptyZone == 0) {
             min = 0;
@@ -54,6 +57,7 @@ public class Main {
         if (min == Integer.MAX_VALUE) {
             min = -1;
         }
+
         bw.write(String.valueOf(min));
         bw.flush();
         bw.close();
@@ -62,60 +66,65 @@ public class Main {
     private static void recursiveSolve(int start, int depth) {
 
         if (depth == M) {
-            List<Integer> combiVirus = new ArrayList<>();
-            for (int c : combi) {
-                combiVirus.add(c);
-            }
-            diffusion(combiVirus, emptyZone);
+            diffusion(emptyZone);
             return;
         }
 
-        for (int i = start; i < totalVirus.size(); i++) {
-            combi[depth] = totalVirus.get(i);
+        for (int i = start; i < virusGroup.size(); i++) {
+            combi[depth] = virusGroup.get(i);
             recursiveSolve(i + 1, depth + 1);
         }
     }
 
-    private static void diffusion(List<Integer> combiVirus, int count) {
-        int day = 0;
+    private static void diffusion(int count) {
+        Queue<Virus> queue = new ArrayDeque<>();
         visited = new boolean[N][N];
-        for (int c : combiVirus) {
-            visited[c / N][c % N] = true;
+        for (Virus virus : combi) {
+            queue.offer(virus);
+            visited[virus.r][virus.c] = true;
         }
-        Queue<List<Integer>> queue = new LinkedList<>();
-        queue.offer(combiVirus);
 
         while (!queue.isEmpty()) {
-            List<Integer> virus = queue.poll();
-            List<Integer> tmpVirus = new ArrayList<>();
+            Virus virus = queue.poll();
+            int r = virus.r;
+            int c = virus.c;
 
-            for (int v : virus) {
-                int r = v / N;
-                int c = v % N;
-                for (int[] direction : directions) {
-                    int newR = r + direction[0];
-                    int newC = c + direction[1];
-                    if (!(newR >= 0 && newR < N && newC >= 0 && newC < N)) {
-                        continue;
-                    }
-                    if (visited[newR][newC] || mapView[newR][newC] == 1) {
-                        continue;
-                    }
-                    if (mapView[newR][newC] == 0) {
-                        count--;
-                    }
-                    if (count == 0) {
-                        min = Math.min(min, day + 1);
-                        return;
-                    }
-                    visited[newR][newC] = true;
-                    tmpVirus.add(newR * N + newC);
+            for (int[] direction : directions) {
+                int newR = r + direction[0];
+                int newC = c + direction[1];
+
+                if (!(newR >= 0 && newR < N && newC >= 0 && newC < N)) {
+                    continue;
                 }
+
+                if (visited[newR][newC] || mapView[newR][newC] == 1) {
+                    continue;
+                }
+
+                if (mapView[newR][newC] == 0) {
+                    count--;
+                }
+
+                if (count == 0) {
+                    min = Math.min(min, virus.day + 1);
+                    return;
+                }
+                visited[newR][newC] = true;
+                queue.add(new Virus(newR, newC, virus.day + 1));
             }
-            if (!tmpVirus.isEmpty()) {
-                day++;
-                queue.offer(tmpVirus);
-            }
+        }
+    }
+
+    static class Virus {
+
+        int r;
+        int c;
+        int day;
+
+        public Virus(int r, int c, int day) {
+            this.r = r;
+            this.c = c;
+            this.day = day;
         }
     }
 }
