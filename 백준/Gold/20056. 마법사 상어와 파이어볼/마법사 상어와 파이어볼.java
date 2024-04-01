e79f90b73245fc49bd66d433f4123fad84b5eb06
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-04-01 23:21:51 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 492 ms, Memory: 54284 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
@@ -1,198 +1,177 @@
-// 0. 파이어볼은 d의 방향으로 s(속력)칸 이동
-// 1. 1번 행은 N번과 연결되어 있고, 1번 열은 N번 열과 연결되어 있다. 즉, N -> 1 로 갈수 있다는 뜻
-// 2. 파이어볼이 같은 칸에 있을 경우 파이어볼은 합쳐진뒤, 4개의 파이어볼로 나누어진다.
-//  -> 질량 = 합쳐진 파이어볼 질량의 합 / 5
-//  -> 속력 = 합쳐진 파이어볼 속력의 합 / 합쳐진 파이어볼의 개수
-// 3. 합쳐지는 파이어볼의 방향이 모두 홀수이거나 모두 짝수이면, 뱡향은 0,2,4,6이 되고, 그렇지 않으면 1,3,5,7이 된다.
-// 4. 질량이 0인 파이어볼은 소멸되어 없어진다.
-// 5. K 번 명령한 후 , 남아있는 파이어볼 질량의 합 구하기
-
-// - 일때 반시계 , + 시계
-//int[N][N][4] 각 r,c에 {m,s,d,c} c는 해당칸의 파이볼개수
-//파이어볼 그룹을 순회하면서 이동 시키기, 이동한 칸에 m,s,d 더하기
-//파이어볼이 모두 이동하면 맵 전체를 돌면서 c >= 2인 곳인경우는 파이어볼 나눠주기
-
-
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
+	static int map[][][];
+	static Queue<Integer[]> fireball =new LinkedList<>();
+	static int dy[] = {-1,-1,0,1,1,1,0,-1};
+	static int dx[] = {0,1,1,1,0,-1,-1,-1};
+	static Queue<Integer> direction[][];
+	public static void main(String[] args) throws IOException{
 
-    private static BufferedReader br;
-    private static BufferedWriter bw;
-    private static StringTokenizer st;
-    private static int N;
-    private static int M;
-    private static int K;
-    private static int[][][] mapView;
-    private static Queue<FireBall> fireBalls;
-    private static Queue<Integer>[][] fireBallDirections;
-    private static int[][] directions;
-    private static int answer;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static class FireBall {
+		StringTokenizer st;
+		st=new StringTokenizer(br.readLine());
+		int N=Integer.parseInt(st.nextToken());
+		int M=Integer.parseInt(st.nextToken());
+		int K=Integer.parseInt(st.nextToken());
 
-        int r;
-        int c;
-        int m;
-        int s;
-        int d;
+		//맵에 3가지 정보를 담는다. 파이어볼의 개수, 파이어볼의 합계질량, 파이어볼의 합계속도
+		map = new int[N][N][3];
+		
+		//해당 위치의 방향의 짝수, 홀수 여부를 알기 위함
+		direction=new LinkedList[N][N];
+	
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				direction[i][j]=new LinkedList<>();
+			}
+		}
 
-        public FireBall(int r, int c, int m, int s, int d) {
-            this.r = r;
-            this.c = c;
-            this.m = m;
-            this.s = s;
-            this.d = d;
-        }
-    }
+		//파이어볼의 정보를 queue에 담는다.
+		for(int i=0;i<M;i++) {
+			st=new StringTokenizer(br.readLine());
+			int r=Integer.parseInt(st.nextToken());
+			int c=Integer.parseInt(st.nextToken());
+			int m=Integer.parseInt(st.nextToken());
+			int s=Integer.parseInt(st.nextToken());
+			int d=Integer.parseInt(st.nextToken());
+			fireball.add(new Integer[] {r-1,c-1,m,s,d});
+		}
 
-    public static void main(String[] args) throws IOException {
+		//파이어볼을 이동시키고
+		//파이어볼의 위치를 조정한다.
+		for(int i=0;i<K;i++) {
+			moveFireball();
+			replaceFireball();
+		}
 
-        init();
-
-        solve();
-
-        printAnswer();
-    }
-
-    private static void printAnswer() throws IOException {
-
-        bw.write(String.valueOf(answer));
-        bw.flush();
-        bw.close();
-    }
-
-    private static void solve() {
-
-        while (K-- > 0) {
-            moveFireBall();
-            divideFireBall();
-        }
-        answer = 0;
-
-        while (!fireBalls.isEmpty()) {
-            FireBall fireBall = fireBalls.poll();
-            answer += fireBall.m;
-        }
-    }
-
-    private static void divideFireBall() {
-
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                int fireCount = mapView[r][c][2];
-                if (fireCount == 0) {
-                    continue;
-                }
-                int m = mapView[r][c][0];
-                int s = mapView[r][c][1];
-                if (fireCount >= 2) {
-                    int newM = m / 5;
-                    int newS = s / fireCount;
-
-                    int flagNum = fireBallDirections[r][c].peek() % 2;
-                    boolean flag = true;
-                    while (!fireBallDirections[r][c].isEmpty()) {
-                        if (flagNum != fireBallDirections[r][c].poll() % 2) {
-                            flag = false;
-                        }
-                    }
-
-                    if (newM == 0) {
-                        continue;
-                    }
-
-                    if (flag) {
-                        for (int newD : new int[]{0, 2, 4, 6}) {
-                            fireBalls.offer(new FireBall(r, c, newM, newS, newD));
-                        }
-                    } else {
-                        for (int newD : new int[]{1, 3, 5, 7}) {
-                            fireBalls.offer(new FireBall(r, c, newM, newS, newD));
-                        }
-                    }
-                } else {
-                    fireBalls.offer(new FireBall(r, c, m, s, fireBallDirections[r][c].poll()));
-                }
-                mapView[r][c][0] = 0;
-                mapView[r][c][1] = 0;
-                mapView[r][c][2] = 0;
-
-            }
-        }
-    }
-
-    private static void moveFireBall() {
-        
-        mapView = new int[N][N][3];
-
-        while (!fireBalls.isEmpty()) {
-            FireBall fireBall = fireBalls.poll();
-            int r = fireBall.r;
-            int c = fireBall.c;
-            int m = fireBall.m;
-            int s = fireBall.s;
-            int d = fireBall.d;
-
-            if (directions[d][0] < 0) {
-                r = (N + (r + directions[d][0] * s)) % N;
-                if (r < 0) {
-                    r += N;
-                }
-            }
-
-            if (directions[d][0] > 0) {
-                r = (r + directions[d][0] * s) % N;
-            }
-
-            if (directions[d][1] < 0) {
-                c = (N + (c + directions[d][1] * s)) % N;
-                if (c < 0) {
-                    c += N;
-                }
-            }
-
-            if (directions[d][1] > 0) {
-                c = (c + directions[d][1] * s) % N;
-            }
-
-            mapView[r][c][0] += m;
-            mapView[r][c][1] += s;
-            mapView[r][c][2] += 1;
-            fireBallDirections[r][c].offer(d);
-        }
-    }
+		//k번 함수가 시행되면 파이어볼의 개수를 세어준다
+		int sum = countFireball();
 
 
-    private static void init() throws IOException {
+		//합계 출력
+		System.out.println(sum);
 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        mapView = new int[N][N][3];
-        fireBalls = new ArrayDeque<>();
-        fireBallDirections = new ArrayDeque[N][N];
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                fireBallDirections[r][c] = new ArrayDeque<>();
-            }
-        }
-        directions = new int[][]{{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
+	}
+	
+	//파이어볼을 이동시키는 함수
+	public static void moveFireball() {
+		
+		while(!fireball.isEmpty()) {
+			Integer temp[]= fireball.poll();
+			int r = temp[0];
+			int c = temp[1];
+			int m = temp[2];
+			int s = temp[3];
+			int d = temp[4];
+			
+			//1칸씩 이동시키지 않고, 나머지 연산으로 다음 위치를 구한다.
+			//(1칸씩 이동시킨다면 시간 초과가 날 것으로 예상)
+			r += (dy[d] * s);
+			c += (dx[d] * s);
+			r%=map.length;
+			c%=map.length;
+			
+			//다음 위치가 음수가 된 경우는 따로 처리
+			if(r<0) {
+				r = Math.abs(r);
+				r = map.length - r;
+			}
+			if(c<0) {
+				c=Math.abs(c);
+				c=map.length - c;
+			}
+			
+			map[r][c][0] += 1; //개수 
+			map[r][c][1] += m; //질량
+			map[r][c][2] += s; //속도
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int s = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            fireBalls.offer(new FireBall(r - 1, c - 1, m, s, d));
-        }
-    }
+
+			//방향을 저장해준다.
+			direction[r][c].add(d);
+
+		}
+
+
+	}
+	//파이어볼의 위치 조정
+	public static void replaceFireball() {
+
+		for(int i=0;i<map.length;i++) {
+			for(int j=0;j<map.length;j++) {
+				int sum = map[i][j][0];  //파이어볼 개수
+				int m = map[i][j][1]; //파이어볼 합계 질량
+				int s = map[i][j][2]; //파이어볼 합계 속력
+		
+				//해당 위치에 파이어볼이 1개라면 그냥 다시 똑같이 add해준다.
+				if(map[i][j][0]==1) {
+					int now = direction[i][j].poll();
+					fireball.add(new Integer[] {i,j,m,s,now});
+				}
+				
+				//해당 위치에 파이어볼이 2개 이상이라면
+				else if(map[i][j][0]>1) {
+					//새로운 크기, 속도
+					int newM = m / 5;
+					int newS= s / sum ;
+					
+					//기본 방향은 0,2,4,6
+					int dir[]= {0,2,4,6};
+					boolean findOdd= false;
+					boolean findEven=false;
+					//해당 좌표에 저장된 방향을 전부 poll해준다.
+				
+					while(!direction[i][j].isEmpty()) {
+						int poll=direction[i][j].poll();
+						if(poll%2==0) findEven = true;
+						else findOdd = true;
+					}
+					
+					//홀수도 찾고, 짝수도 찾았다면 1,3,5,7로 조정
+					if(findOdd==true&&findEven==true) {
+						dir[0] = 1;
+						dir[1] = 3;
+						dir[2] = 5;
+						dir[3] = 7;
+					}
+
+					//만약 새로운 합계가 0이라면 추가해주지 않아도 된다.
+					//그러므로 1이상인 경우에만 추가해준다.
+					if(newM!= 0) {
+						for(int k=0;k<4;k++) {
+							fireball.add(new Integer[] {i,j,newM,newS,dir[k]});
+						}
+
+					}
+					
+
+				}
+
+				//해당 맵의 파이어볼을 없애준다.
+				map[i][j][0] = 0;
+				map[i][j][1] = 0;
+				map[i][j][2] = 0;
+			
+				
+			}
+		}
+
+	}
+	//파이어볼을 세어주는 함수
+	public static int countFireball() {
+
+		int sum = 0;
+		///temp[2]가 질량이므로 질량을 다 더해준다.
+		while(!fireball.isEmpty()) {
+			Integer temp[]= fireball.poll();
+			sum += temp[2];
+		}
+
+		return sum;
+	}
+
 }
-
