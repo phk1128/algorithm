1cf9b3378a41f587751c9bcf63370956b100a4a6
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-04-08 21:02:59 +0900

    [Gold IV] Title: 타임머신, Time: 292 ms, Memory: 19368 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,107 +1,107 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static BufferedReader br;
-    private static BufferedWriter bw;
-    private static StringTokenizer st;
-    private static List<Edge> graph;
-    private static long[] cost;
-
-    public static void main(String[] args) throws IOException {
-
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        graph = new ArrayList<>();
-        cost = new long[n + 1];
-        Arrays.fill(cost, Integer.MAX_VALUE);
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-
-            graph.add(new Edge(start, end, weight));
-        }
-
-        if (bellManFord(n, 1)) {
-            for (int i = 2; i <= n; i++) {
-                if (cost[i] == Integer.MAX_VALUE) {
-                    cost[i] = -1;
-                }
-                bw.write(cost[i] + "\n");
-                bw.flush();
-            }
-        } else {
-            bw.write(String.valueOf(-1));
-            bw.flush();
-        }
-
-        bw.close();
-
-    }
-
-    private static boolean bellManFord(int n, int s) {
-        cost[s] = 0;
-
-        for (int i = 1; i <= n; i++) {
-            for (Edge edge : graph) {
-                int start = edge.getStart();
-                int end = edge.getEnd();
-                int weight = edge.getWeight();
-
-                if (cost[start] == Integer.MAX_VALUE) {
-                    continue;
-                }
-
-                if (cost[end] > cost[start] + weight) {
-                    cost[end] = cost[start] + weight;
-
-                    if (i == n) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
-    static class Edge implements Comparable<Edge> {
-
-        private int start;
-        private int end;
-        private int weight;
-
-        public Edge(int start, int end, int weight) {
-            this.start = start;
-            this.end = end;
-            this.weight = weight;
-        }
-
-        public int getStart() {
-            return this.start;
-        }
-
-        public int getEnd() {
-            return this.end;
-        }
-
-        public int getWeight() {
-            return this.weight;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.weight - o.weight;
-        }
-
-
-    }
-
+	
+	private static BufferedReader br;
+	private static BufferedWriter bw;
+	private static StringTokenizer st;
+	private static List<Edge> graph;
+	private static long[] dist;
+	
+	
+	static class Edge {
+		
+		int start;
+		int end;
+		int weight;
+		
+		public Edge(int start, int end, int weight) {
+			this.start = start;
+			this.end = end;
+			this.weight = weight;
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		dist = new long[N + 1];
+		graph = new ArrayList<>();
+		
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			
+			graph.add(new Edge(start,end,weight));
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		if (!bellManFord(N, 1)) {
+			
+			for (int i = 2; i <= N; i++) {
+				long d = dist[i];
+				
+				if (d == Integer.MAX_VALUE) {
+					d = -1;
+				}
+				sb.append(d);
+				sb.append("\n");
+			
+			}
+		} else {
+			sb.append(-1);
+			sb.append("\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+	
+	private static boolean bellManFord(int N, int s) {
+		
+		dist[s] = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			for (Edge edge : graph) {
+				
+				int start = edge.start;
+				int end = edge.end;
+				int weight = edge.weight;
+				
+				
+				// 갈 수 없던 곳이면 패스
+				if (dist[start] == Integer.MAX_VALUE) {
+					continue;
+				}
+				
+				if (dist[end] > dist[start] + weight) {
+					dist[end] = dist[start] + weight;
+					if (i == N) {
+						return true;
+					}
+				}
+			}
+		}
+		
+		return false;
+		
+		
+	}
 }
