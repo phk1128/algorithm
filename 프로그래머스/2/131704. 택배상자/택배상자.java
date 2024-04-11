f7dfb2da405ee7780fcd7290f351513714d14806
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-04-11 22:41:50 +0900

    [level 2] Title: 택배상자, Time: 172.87 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -5,14 +5,13 @@ class Solution {
         int answer = 0;
         
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
         
         int idx = 0;
         for (int i = 1; i <= order.length; i++) {
             
             stack.push(i);
             
-            while (!stack.isEmpty() && idx < order.length) {
+            while (!stack.isEmpty()) {
                 if (stack.peek() == order[idx]) {
                     stack.pop();
                     idx++;
