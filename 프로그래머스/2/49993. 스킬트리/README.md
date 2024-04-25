23b0659f8dbd81656c4a845c7447b11ac3a1faf1
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-04-25 23:50:49 +0900

    [level 2] Title: 스킬트리, Time: 10.52 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#qna) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 0.62 ms
+메모리: 78 MB, 시간: 10.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 22:09:21
+2024년 04월 25일 23:50:47
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,42 +1,13 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        Queue<Character> queue = new ArrayDeque<>();
-        
-        for (int i = 0; i < skill.length(); i++) {
-            queue.offer(skill.charAt(i));
-        }
-        
-        for (String skill_tree : skill_trees) {
-            Queue<Character> tmpQ = new ArrayDeque<>(queue);
-            for (int i = 0; i < skill_tree.length(); i++) {
-                
-                if (tmpQ.isEmpty()) {
-                    continue;
-                }
-                
-                char tmp = tmpQ.peek();
-                
-                if (Objects.equals(tmp, skill_tree.charAt(i))) {
-                    tmpQ.poll();
-                }
-            }
-            
-            if (tmpQ.isEmpty()) {
-                answer++;
-            } else {
-                answer++;
-                while (!tmpQ.isEmpty()) {
-                    if (skill_tree.contains(String.valueOf(tmpQ.poll()))) {
-                        answer--;
-                        break;
-                    }
-                }
-            }
-        }
-        
+        int answer = -1;
+        String regex = "[^" + skill + "]";
+        answer = (int) Arrays
+            .stream(skill_trees)
+            .filter(tree -> skill.startsWith(tree.replaceAll(regex, ""))).count();
         return answer;
     }
 }
