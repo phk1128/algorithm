b576f1fedac10e2520f1cd987806952cc0caefbb
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-04-29 19:55:31 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 36.72 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = -1;
+        long sum1 = (long) Arrays.stream(queue1).sum();
+        long sum2 = (long) Arrays.stream(queue2).sum();
+        long total = sum1 + sum2;
+        
+        if (total % 2 != 0) {
+            return -1;
+        }
+        
+        int limit = (int) Math.pow(10, 8);
+        long number = total / 2;
+        Queue<Long> q1 = new ArrayDeque<>();
+        Queue<Long> q2 = new ArrayDeque<>();
+        
+        for (int i = 0; i < queue1.length; i++) {
+            q1.offer((long)queue1[i]);
+            q2.offer((long)queue2[i]);
+        }
+        int count = 0;
+        while (limit-- > 0) {
+            if (sum1 > sum2) {
+                if (!q1.isEmpty()) {
+                    long num1 = q1.poll();
+                    sum1 -= num1;
+                    sum2 += num1;
+                    q2.offer(num1);
+                    count++;
+                }
+            }
+            if (sum1 < sum2) {
+                if (!q2.isEmpty()) {
+                    long num2 = q2.poll();
+                    sum1 += num2;
+                    sum2 -= num2;
+                    q1.offer(num2);
+                    count++;
+                }
+            }
+            if (sum1 == sum2) {
+                return count;
+            }
+        }
+        
+        return answer;
+    }
+}
