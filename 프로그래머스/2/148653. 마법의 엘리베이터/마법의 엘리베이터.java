2cb91a86360d1a719f75889f87fe6a62e5bc8366
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-04-30 21:54:11 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.07 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -4,48 +4,32 @@ class Solution {
     private static int answer;
     public int solution(int storey) {
         answer = 0;
-        recursiveSolve(storey, 0, 1);
+        recursiveSolve(storey, 0);
         
         return answer;
     }
     
-    private static void recursiveSolve(int storey, int count, int depth) {
+    private static void recursiveSolve(int storey, int count) {
         
         if (storey == 0) {
             answer = count;
             return;
         }
-        
-        String str = String.valueOf(storey);
-        int len = str.length();
-        int num = Integer.parseInt(String.valueOf(str.charAt(len - depth)));
-        int pow = (int) Math.pow(10, depth - 1);
+        int num = storey % 10;
+        storey /= 10;
         
         if (num == 5) {
-            if (depth < len) {
-                int nextNum = Integer.parseInt(String.valueOf(str.charAt(len - depth - 1)));
-                if (nextNum >= 5) {
-                    storey += (num * pow);
-                } else {
-                    storey -= (num * pow);
-                }
+            if (storey % 10 >= 5) {
+                storey++;
             }
-            if (depth == len) {
-                storey -= (num * pow);
-            }
-        }
-        
-        if (num < 5) {
-            storey -= (num * pow);
         }
         
         if (num > 5) {
             num = 10 - num;
-            storey += (num * pow);
+            storey++;
         }
         
-            
-        recursiveSolve(storey, count + num, depth + 1);
+        recursiveSolve(storey, count + num);
         
     }
 }
