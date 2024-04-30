441374f7a4a7a686d83aac1e611f3722b716a0b2
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-04-30 14:21:52 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.04 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        int[] arr = new int[]{4,1,2};
+        
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            int q = n / 3;
+            int r = n % 3;
+            sb.append(arr[r]);
+            n /= 3;
+            if (r == 0) {
+                n--;
+            }
+        }
+        
+        answer = sb.reverse().toString();
+        
+        
+        return answer;
+    }
+}
