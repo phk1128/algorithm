b807b0ff16ceccd01a2639f047ab2fbbb2167ad2
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-05-02 15:26:50 +0900

    [level 2] Title: 무인도 여행, Time: 2.74 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -6,13 +6,12 @@ class Solution {
     private boolean[][] visited;
     private int[][] mapView;
     private int[][] directions;
-    
+        
     public int[] solution(String[] maps) {
-        int[] answer = {};
+        int[] answer = new int[]{-1};
         visited = new boolean[maps.length][maps[0].length()];
         mapView = new int[maps.length][maps[0].length()];
-        directions = new int[][]{{1,0}, {-1,0}, {0,1}, {0,-1}};
-        
+        directions = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
         
         for (int r = 0; r < maps.length; r++) {
             for (int c = 0; c < maps[0].length(); c++) {
@@ -22,48 +21,54 @@ class Solution {
                 }
             }
         }
-        List<Integer> result = new ArrayList<>();
+        
+        List<Integer> scores = new ArrayList<>();
         for (int r = 0; r < mapView.length; r++) {
             for (int c = 0; c < mapView[0].length; c++) {
                 if (visited[r][c] || mapView[r][c] == 0) {
                     continue;
                 }
-                int score = getScore(r,c,0);
-                if (score > 0) {
-                    result.add(score);
-                }
+                scores.add(getScore(r,c));
             }
         }
         
-        if (!result.isEmpty()) {
-            Collections.sort(result);
-            answer = result.stream().mapToInt(num -> num).toArray();
-        } else {
-            answer = new int[]{-1};
+        if (!scores.isEmpty()) {
+            Collections.sort(scores);
+            answer = scores.stream().mapToInt(num -> num).toArray();
         }
         
         return answer;
     }
     
-    private int getScore(int r, int c, int count) {
-        
+    public int getScore(int r, int c) {
+        int score = mapView[r][c];
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{r,c});
         visited[r][c] = true;
-        count += mapView[r][c];
         
-        for (int[] direction : directions) {
-            int newR = r + direction[0];
-            int newC = c + direction[1];
+        while (!queue.isEmpty()) {
             
-            if (!(newR >= 0 && newR < mapView.length && newC >= 0 && newC < mapView[0].length)) {
-                continue;
+            int[] current = queue.poll();
+            int cR = current[0];
+            int cC = current[1];
+        
+            for (int[] direction : directions) {
+                int nR = cR + direction[0];
+                int nC = cC + direction[1];
+                if (!(nR >= 0 && nR < mapView.length && nC >= 0 && nC < mapView[0].length)) {
+                    continue;
+                }
+                
+                if (visited[nR][nC] || mapView[nR][nC] == 0) {
+                    continue;
+                }
+                visited[nR][nC] = true;
+                score += mapView[nR][nC];
+                queue.offer(new int[]{nR,nC});
             }
-            if (visited[newR][newC] || mapView[newR][newC] == 0) {
-                continue;
-            }
-            count = getScore(newR, newC, count);
+            
         }
         
-        return count;
-        
+        return score;
     }
 }
