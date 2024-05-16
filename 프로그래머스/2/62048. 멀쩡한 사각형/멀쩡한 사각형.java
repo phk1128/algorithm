03feeae3d385867c080d260e57646bfeab8a319f
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-05-16 19:07:32 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.07 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int w, int h) {
+        long answer = 0;
+        long x = (long) w;
+        long y = (long) h;
+        for (int i = 1; i < w; i++) {
+            answer += (y * i) / x;
+        }
+        return answer * 2;
+    }
+}
