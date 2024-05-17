d7a8e4e9f3e04b5eb00e22264f66592d59124aa1
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-05-17 21:06:16 +0900

    [level 2] Title: 광물 캐기, Time: 441.21 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,81 +1,54 @@
 import java.util.*;
 
 class Solution {
+    private Map<String,Integer> mineralKind;
+    private int[][] costs;
+    private int[] combi;
+    private int answer;
     public int solution(int[] picks, String[] minerals) {
-        int answer = 0;
-        int picksCount = (int) Arrays.stream(picks).sum();
-        int len = (int) Math.ceil(minerals.length / 5.0);
-        String[][] mineralGroup = new String[len][5];
-        int[][] costs = new int[len][2];
-        int[] select = new int[len];
+        answer = Integer.MAX_VALUE;
+        mineralKind = new HashMap<>();
+        costs = new int[][]{{1,1,1}, {5,1,1}, {25,5,1}};
+        mineralKind.put("diamond", 0);
+        mineralKind.put("iron", 1);
+        mineralKind.put("stone", 2);
+        int picksCount = Arrays.stream(picks).sum();
+        combi = new int[picksCount];
         
-        for (int i = 0; i < picksCount * 5; i++) {
-            if (i >= minerals.length) {
-                break;
-            }
-            String mineral = minerals[i];
-            int cost = 0;
-            if (Objects.equals(mineral, "diamond")) {
-                cost = 25;
-            }
-            if (Objects.equals(mineral, "iron")) {
-                cost = 5;
-            }
-            if (Objects.equals(mineral, "stone")) {
-                cost = 1;
-            }
-            costs[i / 5][1] += cost;
-            costs[i / 5][0] = i / 5;
-            mineralGroup[i / 5][i % 5] = mineral;
+        recursiveSolve(0,picksCount,picks,minerals);
+        
+        return answer;
+    }
+    private void recursiveSolve(int depth, int limit, int[] picks, String[] minerals) {
+        if (depth == limit) {
+            answer = Math.min(answer, getCost(minerals));
+            return;
         }
         
-        Arrays.sort(costs, (c1,c2) -> c2[1] - c1[1]);
-        
-        
-        for (int[] cost : costs) {
-            if (cost[1] == 0) {
-                break;
+        for (int i = 0; i < picks.length; i++) {
+            if (picks[i] <= 0) {
+                continue;
             }
-            int tmpPick = -1;
-            for (int i = 0; i < picks.length; i++) {
-                if (picks[i] > 0) {
-                    picks[i]--;
-                    tmpPick = i; 
+            combi[depth] = i;
+            picks[i]--;
+            recursiveSolve(depth + 1, limit, picks, minerals);
+            picks[i]++;
+        }
+    }
+    
+    private int getCost(String[] minerals) {
+        int idx = 0;
+        int cost = 0;
+        for (int i = 0; i < combi.length; i++) {
+            int pick = combi[i];
+            int count = 5;
+            while (count-- > 0) {
+                if (idx == minerals.length) {
                     break;
                 }
-            }
-            select[cost[0]] = tmpPick;
-        }
-           
-        for (int i = 0; i < mineralGroup.length; i++) {
-            int pickNum = select[i];
-            for (String mineral : mineralGroup[i]) {
-                if (Objects.equals(mineral, null)) {
-                    continue;
-                }
-                if (pickNum == 0) {
-                    answer++;
-                }
-                if (pickNum == 1) {
-                    if (Objects.equals(mineral, "diamond")) {
-                        answer += 5;
-                        continue;
-                    }
-                    answer++;
-                }
-                if (pickNum == 2) {
-                    if (Objects.equals(mineral, "diamond")) {
-                        answer += 25;
-                        continue;
-                    }
-                    if (Objects.equals(mineral, "iron")) {
-                        answer += 5;
-                        continue;
-                    }
-                    answer++;
-                }
+                cost += costs[pick][mineralKind.get(minerals[idx++])];
             }
         }
-        return answer;
+        return cost;
     }
 }
