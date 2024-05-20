ce9b45cf1e3836402801a7525581f90b2baad5d5
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-05-20 15:07:42 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 12.90 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class Solution {
+    
+    private Queue<Integer> bus;
+    private PriorityQueue<Integer> crews;
+    
+    public String solution(int n, int t, int m, String[] timetable) {
+        String answer = "";
+        crews = new PriorityQueue<>();
+        bus = new ArrayDeque<>();
+        
+        for (int i = 0; i < timetable.length; i++) {
+            String[] timeSplit = timetable[i].split(":");
+            crews.offer((Integer.parseInt(timeSplit[0]) * 60) + Integer.parseInt(timeSplit[1]));
+        }
+        
+        int time = 540;
+        for (int i = 0; i < n; i++) {
+            bus.offer(time);
+            time += t;
+        }
+        
+        answer = getAnswer(n,m);
+        
+        return answer;
+    }
+    
+    private String getAnswer(int n, int m) {
+        
+        int lastBus = 0;
+        int lastCrew = 0;
+        int lastCount = 0;
+        System.out.println(bus);
+        while (!bus.isEmpty()) {
+            int busTime = bus.poll();
+            int count = 0;
+            while (!crews.isEmpty() && crews.peek() <= busTime) {
+                int crewTime = crews.poll();
+                count++;
+                lastCrew = crewTime;
+                if (count >= m) {
+                    break;
+                }
+            }
+            lastBus = busTime;
+            lastCount = count;
+        }
+        
+        if (lastCrew == 0 || lastCount < m) {
+            return convertToDateTimeFormat(lastBus);
+        }
+        
+        return convertToDateTimeFormat(lastCrew - 1);
+    }
+    
+    private String convertToDateTimeFormat(int time) {
+        int h = time / 60;
+        int m = time % 60;
+        String hStr = String.valueOf(h);
+        String mStr = String.valueOf(m);
+        if (h < 10) {
+            hStr = "0" + hStr;
+        }
+        if (m < 10) {
+            mStr = "0" + mStr;
+        }
+        return hStr + ":" + mStr;
+    }
+    
+    
+}
+
+// 전부 분단위로 연산하기
+// 버스를 큐에 담아 놓기
+// 큐에서 버스를 꺼내서 크루원들 태우기
+// 큐에 버스가 하나 남았으면, 콘이 탈 수 있는지 체크 탈수 없으면 전에 있던 버스에 태우기
+// 23:59에 줄스고 있는 크루는 무시
+// 마지막 버스를 탄 크루원 보다 1분 더 일찍 타기
+// 만약 마지막 버스를 탄 크루원이 0 이면 막차에 타기
