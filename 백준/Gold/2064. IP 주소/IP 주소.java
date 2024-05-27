402ff0623f341e18be25975b18b350d9beb55285
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-05-27 14:47:36 +0900

    [Gold III] Title: IP 주소, Time: 148 ms, Memory: 15404 KB -BaekjoonHub

--- 백준/Gold/2064. IP 주소/IP 주소.java
+++ 백준/Gold/2064. IP 주소/IP 주소.java
@@ -1,73 +1,56 @@
+// TODO: 87% fail
 import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
 
-class Main {
-
-    private static BufferedReader br;
-    private static BufferedWriter bw;
-    private static StringTokenizer st;
-    private static String[][] addressGroup;
-
-    public static void main(String[] args) throws IOException {
-
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        addressGroup = new String[n][4];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            addressGroup[i] = st.nextToken().split("\\.");
-        }
-
-        StringBuilder[] result = getResult(n);
+public class Main {
+    static String toString(int[] ip) {
         StringBuilder sb = new StringBuilder();
-        sb.append(result[0].toString());
-        sb.append("\n");
-        sb.append(result[1].toString());
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-
+        for(int n:ip) {
+            sb.append(n).append('.');
+        }
+        return sb.toString().substring(0, sb.length()-1);
     }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] ips = new int[N][4];
+        int[] network = new int[4];
+        int[] subnet = new int[4];
 
-    private static StringBuilder[] getResult(int n) {
-
-        StringBuilder[] result = new StringBuilder[2];
-        boolean flag = false;
-        StringBuilder networkAddress = new StringBuilder();
-        StringBuilder networkMask = new StringBuilder();
-        for (int i = 0; i < 4; i++) {
-            int min = Integer.parseInt(addressGroup[0][i]);
-            int max = Integer.parseInt(addressGroup[0][i]);
-            for (int j = 1; j < n; j++) {
-                min &= Integer.parseInt(addressGroup[j][i]);
-                max |= Integer.parseInt(addressGroup[j][i]);
-            }
-
-            if (!flag) {
-                networkAddress.append(min);
-                networkAddress.append(".");
-                networkMask.append(255 - (max - min));
-                networkMask.append(".");
-            } else {
-                networkAddress.append("0");
-                networkAddress.append(".");
-                networkMask.append("0");
-                networkMask.append(".");
-            }
-            if (min != max) {
-                flag = true;
+        for(int i=0; i<N; i++) {
+            String[] ipsStr = br.readLine().split("\\.");
+            for(int j=0; j<4; j++) {
+                ips[i][j] = Integer.parseInt(ipsStr[j]);
             }
         }
 
+        boolean isSubnet = true;
 
-        result[0] = networkAddress.deleteCharAt(networkAddress.length() - 1);
-        result[1] = networkMask.deleteCharAt(networkMask.length() - 1);
-        return result;
+        for(int i=0; i<4; i++) {
+            network[i] = 0;
+            subnet[i] = 0;
+            for(int[] ip:ips) {
+                network[i] = network[i] & ip[i];
+            }
+
+            if(!isSubnet) {
+                continue;
+            }
+            for(int j=7; j>=0; j--) {
+                int bit=1<<j;
+                for(int[] ip:ips) {
+                    if((ips[0][i]&bit) != (ip[i]&bit)) {
+                        isSubnet = false;
+                        break;
+                    }
+                }
+                if(!isSubnet) {
+                    break;
+                }
+                subnet[i] |= bit;
+            }
+            network[i] = ips[0][i] & subnet[i];
+        }
+        System.out.println(toString(network));
+        System.out.println(toString(subnet));
     }
 }
