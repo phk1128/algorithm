9e41cd3cc8b67f9104966f2e94268af5312df68f
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-06-03 16:10:31 +0900

    [level 3] Title: 입국심사, Time: 0.67 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 72.6 MB, 시간: 0.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 03일 15:53:16
+2024년 06월 03일 16:10:29
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+
+    private long minTime;
+    
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long maxTime = (long)times[times.length - 1] * n;
+        binarySearch(1, maxTime, n, times);
+        answer = minTime;
+        
+        return answer;
+    }
+    
+    private void binarySearch(long start, long end, int n, int[] times) {
+        if (start >= end) {
+            minTime = start;
+            return;
+        }
+        
+        long mid = (start + end) / 2;
+        long count = getCount(mid, times, n);
+        
+        if (n > count) {
+            binarySearch(mid + 1, end, n, times);
+        }
+        if (n <= count) {
+            binarySearch(start, mid, n, times);
+        }
+    }
+    
+    private long getCount(long time, int[] times, int n) {
+        long count = 0;
+        
+        for (int i = 0; i < times.length; i++) {
+            count += time / times[i];
+        }
+        return count;
+    }
+}
