ee027679c2e92cc491783eebc08e2ca655143de6
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-06-04 14:52:41 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 2.87 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        
+        int del = 0;
+        int pick = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            del -= deliveries[i];
+            pick -= pickups[i];
+            
+            while (del < 0 || pick < 0) {
+                del += cap;
+                pick += cap;
+                answer += (i + 1) * 2;
+            }
+        }
+        
+        return answer;
+    }
+}
