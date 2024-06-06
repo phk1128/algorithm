b1e5650f7b6a66cf6b7f3b96607fcafc2071f779
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-06-06 16:29:57 +0900

    [level 2] Title: [3차] n진수 게임, Time: 4.37 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] n진수 게임 - 17687 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687) 
 
 ### 성능 요약
 
-메모리: 95.5 MB, 시간: 13.47 ms
+메모리: 73.6 MB, 시간: 4.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 15:35:44
+2024년 06월 06일 16:29:55
 
 ### 문제 설명
 
--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,84 +1,27 @@
-import java.util.*;
-
 class Solution {
-    private static StringBuilder sb;
-    private static String answer;
-    private static int n;
-    private static int t;
-    private static int m;
-    private static int p;
-    private static int turn;
-    private static String[] alpha;
-    
     public String solution(int n, int t, int m, int p) {
-        
-        this.n = n;
-        this.t = t;
-        this.m = m;
-        this.p = p;
-        turn = p % m;
-        
-        alpha = new String[16];
-        
-        for (int i = 10; i < 16; i++) {
-            alpha[i] = String.valueOf((char) (55 + i));
+        String answer = "";
+        int num = 0;
+        int count = 0;
+        boolean flag = true;
+        while(flag) {
+            String str = Integer.toString(num, n);
+            for (int i = 0; i < str.length(); i++) {
+                count++;
+                if (count == p) {
+                    answer += String.valueOf(str.charAt(i)).toUpperCase();
+                    t--;
+                }
+                if (count >= m) {
+                    count = 0;
+                }
+                if (t == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            num++;
         }
-        
-        System.out.println(convert(31, 16, ""));
-        
-        recursiveSolve(0, "", 0, 0);
-        
         return answer;
-    }
-    
-    private static void recursiveSolve(int number, String result, int count, int depth) {
-        
-        String convertedNum = convert(number, n, "");
-        
-        if (number == 0) {
-            convertedNum = "0";
-        }
-        
-        for (int i = 0; i < convertedNum.length(); i++) {
-            count++;
-            if ((count) % m == turn) {
-                depth++;
-                result += String.valueOf(convertedNum.charAt(i));
-            }
-            if (depth == t) {
-                answer = result;
-                return;
-            }
-        }
-        
-        recursiveSolve(number + 1, result, count, depth);
-        
-    }
-    
-    private static String convert(int num, int binary, String result) {
-        
-        
-        if (num == 0) {
-            sb = new StringBuilder(result);
-            return sb.reverse().toString();
-        }
-        
-        if (num % binary != 0) {
-            int tmp = num % binary;
-            String tmpStr = String.valueOf(tmp);
-            if (tmp >= 10 && tmp <= 15) {
-                tmpStr = alpha[tmp];
-            }
-            result = convert(num / binary, binary, result + tmpStr);
-            
-        }
-        
-        if (num % binary == 0) {
-            
-            result = convert(num / binary, binary, result + "0");
-        }
-        
-        return result;
-        
     }
 }
