e3a2e9c7371cb161fc3baa0ad39a1a63b632e7de
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-06-06 19:22:30 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 52.84 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 32.69 ms
+메모리: 106 MB, 시간: 52.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 29일 20:00:37
+2024년 06월 06일 19:22:28
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -4,48 +4,43 @@ import java.util.stream.*;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = -1;
-        long sum1 = (long) Arrays.stream(queue1).sum();
-        long sum2 = (long) Arrays.stream(queue2).sum();
-        long total = sum1 + sum2;
-        
-        if (total % 2 != 0) {
-            return -1;
-        }
-        
-        int limit = queue1.length * 3;
-        long number = total / 2;
-        Queue<Long> q1 = new ArrayDeque<>();
-        Queue<Long> q2 = new ArrayDeque<>();
-        
-        for (int i = 0; i < queue1.length; i++) {
-            q1.offer((long)queue1[i]);
-            q2.offer((long)queue2[i]);
-        }
+        long sum1 = Arrays.stream(queue1).sum();
+        long sum2 = Arrays.stream(queue2).sum();
+        Queue<Integer> q1 = new ArrayDeque<>();
+        Queue<Integer> q2 = new ArrayDeque<>();
+        offerNum(q1, queue1);
+        offerNum(q2, queue2);
+        int limit = (q1.size() + q2.size()) * 2;
         int count = 0;
         while (limit-- > 0) {
-            if (sum1 > sum2) {
-                if (!q1.isEmpty()) {
-                    long num1 = q1.poll();
-                    sum1 -= num1;
-                    sum2 += num1;
-                    q2.offer(num1);
-                    count++;
-                }
+            if (sum1 > sum2 && !q1.isEmpty()) {
+                int num = q1.poll();
+                q2.offer(num);
+                sum1 -= num;
+                sum2 += num;
+                count++;
             }
-            if (sum1 < sum2) {
-                if (!q2.isEmpty()) {
-                    long num2 = q2.poll();
-                    sum1 += num2;
-                    sum2 -= num2;
-                    q1.offer(num2);
-                    count++;
-                }
+            
+            if (sum1 < sum2 && !q2.isEmpty()) {
+                int num = q2.poll();
+                q1.offer(num);
+                sum2 -= num;
+                sum1 += num;
+                count++;
             }
+            
             if (sum1 == sum2) {
-                return count;
+                answer = count;
+                break;
             }
         }
         
         return answer;
+    }
+    
+    private void offerNum(Queue<Integer> queue, int[] nums) {
+        for (int num : nums) {
+            queue.offer(num);
+        }
     }
 }
