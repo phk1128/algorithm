2b630ee54b10d8be75d1bc4bba18651f108100fa
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-06-06 15:30:38 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 11.21 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String decimal = getDecimal("", n , k);
+        String[] split = decimal.split("0");
+        
+        for (String num : split) {
+            if (Objects.equals(num, "")) {
+                continue;
+            }
+            if (isPrime(Long.parseLong(num))) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    private String getDecimal(String result, int n, int k) {
+        if (n == 0) {
+            return result;
+        }
+        return getDecimal((n % k) + result, n / k, k);
+    }
+    
+    private boolean isPrime(long num) {
+        if (num == 1 || num == 0) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        for (int i = 2; i <= (int) Math.pow(num, 0.5); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
