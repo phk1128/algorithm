935fc93f4deaa843fe220d36418ee68e6bba6b7b
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-06-07 16:15:54 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 121.48 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,40 +1,36 @@
 import java.util.*;
-import java.util.stream.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public long[] solution(long[] numbers) {
-        long[] answer = {};
-        List<Long> result = new ArrayList<>();
+        long[] answer = new long[numbers.length];
         
-        for (long number : numbers) {
-            
-            StringBuilder bit = new StringBuilder(Long.toBinaryString(number));
-            int idx = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            long num = numbers[i];
+            StringBuilder decimal = new StringBuilder(Long.toString(num, 2));
             boolean flag = false;
-            for (int i = bit.length() - 1; i >= 0; i--) {
-                if (Objects.equals(bit.charAt(i), '0')) {
-                    idx = i;
+            for (int j = decimal.length() - 1; j >= 0; j--) {
+                if (Objects.equals(decimal.charAt(j), '0')) {
+                    decimal.setCharAt(j, '1');
+                    if (j < decimal.length() - 1) {
+                        decimal.setCharAt(j + 1, '0');
+                    }
                     flag = true;
                     break;
                 }
             }
-            
-            if (flag) {
-                bit.setCharAt(idx, '1');
-                if (idx != bit.length() - 1) {
-                    bit.setCharAt(idx+1, '0');
-                }
-            } else {
-                bit.insert(0, '1');
-                bit.setCharAt(1, '0');
+            if (!flag) {
+                num = (num + 1) + num;
+                decimal = new StringBuilder(Long.toString(num, 2));
+                decimal.setCharAt(1, '0');
             }
-            
-            result.add(Long.parseLong(bit.toString(), 2));
-            
-                
+            long result = Long.parseLong(decimal.toString(), 2);
+            answer[i] = result;
         }
-        answer = result.stream().mapToLong(num -> num).toArray();
         return answer;
     }
 }
+// StringBuilder 사용
+// long을 2진수로 바꿔서 뒤에서 부터 탐색
+// 0을 만나면 1로 바꿔주고, 그 바로 뒤에 값을 0 으로 바꿔준다.
+// 0을 만나지 않으면 (현재 값 + 1) + 현재값을 이진수로 바꾼다.
+// 해당값의 인덱스 1 자리를 0 으로 바꾼다.
