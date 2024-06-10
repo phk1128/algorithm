003228f8143464610f56c05c6923b29c929f4a63
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-06-10 14:22:00 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.58 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        new_id = new_id.toLowerCase();
+        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+        new_id = new_id.replaceAll("^\\.", ""); // 문자열 시작
+        new_id = new_id.replaceAll("\\.$", ""); // 문자열 끝
+        if (Objects.equals(new_id, "")) {
+            new_id += "a";
+        }
+        if (new_id.length() >= 16) {
+            new_id = new_id.substring(0, 15);
+            new_id = new_id.replaceAll("\\.$", "");
+        }
+        while (new_id.length() <= 2) {
+            String str = String.valueOf(new_id.charAt(new_id.length() - 1));
+            new_id += str;
+        }
+        answer = new_id;
+        return answer;
+    }
+}
