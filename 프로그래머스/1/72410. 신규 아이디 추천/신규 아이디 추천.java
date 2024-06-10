bf97cd26a3eba8d364692425d06e3a94c2a8f197
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2024-06-10 15:50:34 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.24 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -5,14 +5,14 @@ class Solution {
         String answer = "";
         new_id = new_id.toLowerCase();
         new_id = new_id.replaceAll("[^a-z0-9-_.]","");
-        new_id = new_id.replaceAll("\\.{2,}", ".");
+        new_id = new_id.replaceAll("[.]{2,}", ".");
         new_id = new_id.replaceAll("^[.]|[.]$", "");
         if (Objects.equals(new_id, "")) {
             new_id += "a";
         }
         if (new_id.length() >= 16) {
             new_id = new_id.substring(0, 15);
-            new_id = new_id.replaceAll("\\.$", "");
+            new_id = new_id.replaceAll("[.]$", "");
         }
         while (new_id.length() <= 2) {
             String str = String.valueOf(new_id.charAt(new_id.length() - 1));
