4a3b188d0c150660efc73cfced79881215b65533
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-08-27 23:14:36 +0900

    [Silver IV] Title: 수 찾기, Time: 648 ms, Memory: 43012 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,66 +1,61 @@
-import java.util.*;
-import java.io.*;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main  {
 
-    private static BufferedReader br;
-    private static BufferedWriter bw;
-    private static StringTokenizer st;
+    private static int N, M;
+    private static int[] arr;
+    private static int[] numbers;
 
-    private static List<Integer> numbers;
-    private static List<Integer> targets;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException{
-
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-
-        numbers = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        Stream.iterate(0, i -> i + 1).limit(n).forEach(i -> {
-            numbers.add(Integer.parseInt(st.nextToken()));
-        });
-
-        Collections.sort(numbers);
-
-        st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-
-        targets = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        Stream.iterate(0, i -> i + 1).limit(m).forEach(i -> {
-            targets.add(Integer.parseInt(st.nextToken()));
-        });
-
-        for (int target : targets) {
-            binarySearch(0, n-1, target);
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        bw.close();
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            if (exist(0, N - 1, numbers[i])) {
+                sb.append(1);
+            } else {
+                sb.append(0);
+            }
+            sb.append(System.lineSeparator());
+        }
+        System.out.println(sb);
     }
 
-    private static void binarySearch(int start, int end, int target) throws IOException{
-
-        if (start == end) {
-            if (numbers.get(start) == target) {
-                bw.write("1" + "\n");
-                bw.flush();
-            } else {
-                bw.write("0" + "\n");
-                bw.flush();
-            }
-            return;
+    private static boolean exist(int start, int end, int target) {
+        if (start >= end) {
+            return arr[start] == target;
         }
-
         int mid = (start + end) / 2;
 
-        if (target > numbers.get(mid)) {
-            binarySearch(mid + 1, end, target);
-        } else {
-            binarySearch(start, mid, target);
+        if (arr[mid] < target) {
+            return exist(mid + 1, end, target);
         }
+
+        if (arr[mid] >= target) {
+            return exist(start, mid, target);
+        }
+        return false;
     }
 }
