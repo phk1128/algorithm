74c9395b74c20ae61979a281b2b01d990ddfce03
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-08-31 01:38:59 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 288 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,44 +1,29 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-
     private static BufferedReader br;
-    private static BufferedWriter bw;
     private static StringTokenizer st;
     private static int n;
     private static int[][] mapView;
     private static int answer;
     private static int[][] directions;
-
     public static void main(String[] args) throws IOException {
-
         br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-
         mapView = new int[n][n];
         answer = 0;
         directions = new int[][]{{0, 1}, {1, 1}, {1, 0}};
-
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 mapView[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         solve(0, 1, 0);
-
-        bw.write(String.valueOf(answer));
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
     }
-
     private static void solve(int status, int x, int y) {
-
         if (x == n - 1 && y == n - 1) {
             answer++;
             return;
@@ -60,7 +45,6 @@ public class Main {
         if (flag) {
             solve(2, x + 1, y + 1);
         }
-
         if (status == 0 || status == 2) {
             int newX = x + 1;
             int newY = y;
@@ -70,7 +54,6 @@ public class Main {
                 }
             }
         }
-
         if (status == 1 || status == 2) {
             int newX = x;
             int newY = y + 1;
