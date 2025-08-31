2328439fd7c249506d111418b46b057b570d1d10
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-09-01 00:28:43 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,32 +1,19 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-
     private static BufferedReader br;
-    private static BufferedWriter bw;
     private static StringTokenizer st;
     private static int[] dp;
-
     public static void main(String[] args) throws IOException {
-
         br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-
         dp = new int[1001];
         dp[1] = 1;
         dp[2] = 3;
-
         for (int i = 3; i <= n; i++) {
             dp[i] = ((dp[i-2] * 2) + dp[i-1]) % 10_007;
         }
-
-        bw.write(String.valueOf(dp[n]));
-        bw.flush();
-        bw.close();
+        System.out.println(dp[n]);
     }
-
 }
