7711fec5123ec3ca082b3c75bf2311b6845d1500
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-09-17 00:38:54 +0900

    [level 2] Title: 피로도, Time: 3.31 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,23 @@
+class Solution {
+    private static boolean[] visited;
+    private static int count = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
+        return count;
+    }
+    
+    private void dfs(int depth, int k, int[][] dungeons){
+        for(int i = 0; i < dungeons.length; i++){
+            if(visited[i] || dungeons[i][0] > k) continue;
+            
+            visited[i] = true;
+            dfs(depth+1, k - dungeons[i][1], dungeons);
+            visited[i] = false; // 백트래킹
+        }
+        
+        count = Math.max(count, depth); // 최대 뎁스 구하기
+        
+    }
+}
