8c3460c3d0a45c3f6b91d04f6a7466d70ba8d0f7
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-09-22 23:19:21 +0900

    [level 2] Title: 주차 요금 계산, Time: 12.86 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -3,41 +3,49 @@ import java.util.*;
 class Solution {
     
     public int[] solution(int[] fees, String[] records) {
-        int[] answer = {};
-        TreeMap<String, List<Double>> cars = new TreeMap<>();
+        int[] result = {};
+        TreeMap<String, List<Double>> parkingLogs = new TreeMap<>();
         
         for (int i = 0; i < records.length; i++) {
             String[] record = records[i].split(" ");
-            String[] timeSplit = record[0].split(":");
-            String carNum = record[1];
-            String command = record[2];
-            double time = Double.parseDouble(timeSplit[0]) * 60 + Double.parseDouble(timeSplit[1]);
-            List<Double> carTimes = cars.getOrDefault(carNum, new ArrayList<>());
-            carTimes.add(time);
-            cars.put(carNum, carTimes);
-        }
-        answer = new int[cars.size()];
-        int idx = 0;
-        for (String key : cars.keySet()) {
-            List<Double> carTimes = cars.get(key);
-            if (carTimes.size() % 2 != 0) {
-                carTimes.add(23.0 * 60.0 + 59.0);
-            }
-            int price = fees[1];
-            double usedTime = 0;
-            for (int i = 0; i < carTimes.size() - 1; i += 2) {
-                double inTime = carTimes.get(i);
-                double outTime = carTimes.get(i + 1);
-                usedTime += (outTime - inTime);
-            }
-            double diffTime = usedTime - fees[0];
-            if (diffTime > 0) {
-                price += Math.ceil(diffTime / (double) fees[2]) * fees[3];
-            }
-            answer[idx++] = price;
+            String[] timeTokens = record[0].split(":");
+            String carNumber = record[1];
+            String action = record[2];
+            
+            double timeInMinutes = Double.parseDouble(timeTokens[0]) * 60 + Double.parseDouble(timeTokens[1]);
+            
+            List<Double> timeList = parkingLogs.getOrDefault(carNumber, new ArrayList<>());
+            timeList.add(timeInMinutes);
+            parkingLogs.put(carNumber, timeList);
         }
         
-        return answer;
+        result = new int[parkingLogs.size()];
+        int index = 0;
+        
+        for (String carNumber : parkingLogs.keySet()) {
+            List<Double> timeList = parkingLogs.get(carNumber);
+            
+            if (timeList.size() % 2 != 0) {
+                timeList.add(23.0 * 60.0 + 59.0);
+            }
+            
+            int totalFee = fees[1];
+            double totalParkingTime = 0;
+            
+            for (int i = 0; i < timeList.size() - 1; i += 2) {
+                double entryTime = timeList.get(i);
+                double exitTime = timeList.get(i + 1);
+                totalParkingTime += (exitTime - entryTime);
+            }
+            
+            double excessTime = totalParkingTime - fees[0];
+            if (excessTime > 0) {
+                totalFee += Math.ceil(excessTime / (double) fees[2]) * fees[3];
+            }
+            
+            result[index++] = totalFee;
+        }
+        
+        return result;
     }
 }
-// 차량 번호가 작은 자동차부터 청구할 주차 요금을 차례대로 배열에 담아서 반환
