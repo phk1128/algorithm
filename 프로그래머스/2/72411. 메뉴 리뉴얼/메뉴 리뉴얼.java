f39bee0bf048d66cc1ea0bc947d6429638c56e46
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-10-11 01:47:58 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 15.05 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,18 +1,28 @@
 import java.util.*;
 import java.util.stream.Collectors;
-
 class Solution {
     public List<String> solution(String[] orders, int[] course) { 
         List<String> answer = new ArrayList<>();
         for (int quantity : course) {
             Map<String, Integer> courseMenus = new HashMap<>();
             for (String order : orders) {
-                List<String> combination = new ArrayList<>();
                 List<String> covertedOrder = Arrays.stream(order.split("")).collect(Collectors.toList());
                 Collections.sort(covertedOrder);
-                order = String.join("",covertedOrder);
+                order = String.join("", covertedOrder);
+                List<String> combination = new ArrayList<>();
                 boolean[] visited = new boolean[order.length()];
-                addCombination(order, combination, 0, visited, 0, quantity);
+                int totalCombinations = 1 << order.length();
+                for (int mask = 0; mask < totalCombinations; mask++) {
+                    if (Integer.bitCount(mask) == quantity) {
+                        StringBuilder menus = new StringBuilder();
+                        for (int i = 0; i < order.length(); i++) {
+                            if ((mask & (1 << i)) != 0) {
+                                menus.append(order.charAt(i));
+                            }
+                        }
+                        combination.add(menus.toString());
+                    }
+                }
                 for (String menus : combination) {
                     if (Objects.equals(courseMenus.get(menus), null)) {
                         courseMenus.put(menus, 1);
@@ -20,7 +30,6 @@ class Solution {
                     }
                     courseMenus.replace(menus, courseMenus.get(menus) + 1);
                 }
-                
             }
             if (!courseMenus.isEmpty()) {
                 int maxQuantity = Collections.max(courseMenus.values());
@@ -30,29 +39,9 @@ class Solution {
                         answer.add(entry.getKey());
                     }
                 }
-                
             }
-           
         }
         Collections.sort(answer);
         return answer;
-    }
-    public void addCombination(String order, List<String> combination, int start, boolean[] visited, int depth, int quantity) {
-        if (quantity == depth) {
-            String menus = "";
-            for (int i = 0; i < order.length(); i++) {
-                if (visited[i]) {
-                    menus += order.split("")[i];
-                }
-            }
-            combination.add(menus);
-        }
-        for (int i = start; i < order.length(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                addCombination(order, combination, i+1, visited, depth+1, quantity);
-                visited[i] = false;
-            }
-        }
     }
 }
