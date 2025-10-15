1683e6d9316ac437c740f830b567920fb0e5148c
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-10-16 00:51:20 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,29 +3,23 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer = 1;
-
         while (true) {
-            
             if (Math.abs(a - b) == 1) {
                 if (a > b) {
                     if (a % 2 == 0 && a - 1 == b) {
                         break;
                     }
                 }
-                
                 if (b > a) {
                     if (b % 2 == 0 && b - 1 == a) {
                         break;
                     }
                 }
             }
-            
             a = (a / 2) + (a % 2);
-            b = (b / 2) + (b % 2);
-            
+            b = (b / 2) + (b % 2);   
             answer++;
         }
-
         return answer;
     }
 }
