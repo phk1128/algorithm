e1bccdffee86040ce049a38e07d530f26b298ef3
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-10-18 03:27:20 +0900

    [level 2] Title: 디펜스 게임, Time: 103.82 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     
@@ -7,40 +6,23 @@ class Solution {
     
     public int solution(int n, int k, int[] enemy) {
         answer = 0;
-        binarySearch(0, enemy.length, n, k, enemy);
-        return answer;
-    }
-    
-    private void binarySearch(int start, int end, int n, int k, int[] enemy) {
         
-        if (start >= end) {
-            answer = start;
-            return;
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        int mid = (start + end) / 2;
-        System.out.println(mid);
-        int[] tmpEnemy = Arrays.stream(enemy, 0, mid + 1).toArray();
-        Arrays.sort(tmpEnemy);
-        int tmpN = n;
-        boolean flag = true;
-        
-        for (int i = 0; i < tmpEnemy.length; i++) {
-            if (tmpN >= tmpEnemy[i]) {
-                tmpN -= tmpEnemy[i];
-                continue;
+        for (int i = 0; i < enemy.length; i++) {
+            pq.offer(enemy[i]);
+            
+            if (pq.size() > k) {
+                n -= pq.poll();
             }
             
-            if (k < tmpEnemy.length - i) {
-                flag = false;
+            if (n < 0) {
+                answer = i;
+                return answer;
             }
-            break;
         }
         
-        if (flag){
-            binarySearch(mid + 1, end, n, k, enemy);
-        } else {
-            binarySearch(start, mid, n, k, enemy);
-        }
+        answer = enemy.length;
+        return answer;
     }
 }
