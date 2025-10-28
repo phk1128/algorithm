165c2c92470fb22f21e0f226afe86c4db069399f
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-10-28 17:12:10 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 11.52 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 class Solution {
     
-    private int[][] mapView;
+    private static int[][] mapView;
     
     public int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = new int[queries.length];
-        mapView = new int[rows + 1][columns + 1];
-        
+        int[] answer = {};
+        mapView = new int[rows + 1][ columns + 1];
         int num = 1;
         for (int r = 1; r <= rows; r++) {
             for (int c = 1; c <= columns; c++) {
@@ -16,49 +15,45 @@ class Solution {
         }
         
         int idx = 0;
-        for (int[] query : queries) {
-            answer[idx++] = getMinNum(query);
+        answer = new int[queries.length];
+        for (int[] q : queries) {
+            answer[idx++] = calculateMinNum(q);
         }
-        
         return answer;
     }
     
-    private int getMinNum(int[] query) {
+    private int calculateMinNum(int[] q) {
+        int sR = q[0];
+        int sC = q[1];
+        int eR = q[2];
+        int eC = q[3];
+        int tmp = mapView[sR][sC];
+        int min = Integer.MAX_VALUE;
         
-        int num = Integer.MAX_VALUE;
-        int startR = query[0];
-        int startC = query[1];
-        int endR = query[2];
-        int endC = query[3];
-        int tmp = mapView[startR][startC];
-        
-        // 왼쪽
-        for (int r = startR + 1; r <= endR; r++) {
-            num = Math.min(num, mapView[r][startC]);
-            mapView[r - 1][startC] = mapView[r][startC];
+        for (int r = sR; r < eR; r++) {
+            min = Math.min(min, mapView[r][sC]);
+            mapView[r][sC] = mapView[r + 1][sC];
         }
         
-        // 아래
-        for (int c = startC + 1; c <= endC; c++) {
-            num = Math.min(num, mapView[endR][c]);
-            mapView[endR][c - 1] = mapView[endR][c];
+        for (int c = sC; c < eC; c++) {
+            min = Math.min(min, mapView[eR][c]);
+            mapView[eR][c] = mapView[eR][c + 1];
         }
         
-        // 오른쪽
-        for (int r = endR - 1; r >= startR; r--) {
-            num = Math.min(num, mapView[r][endC]);
-            mapView[r + 1][endC] = mapView[r][endC];
+        for (int r = eR; r > sR; r--) {
+            min = Math.min(min, mapView[r][eC]);
+            mapView[r][eC] = mapView[r - 1][eC];
         }
         
-        // 위 
-        for (int c = endC - 1; c >= startC + 1; c--) {
-            num = Math.min(num, mapView[startR][c]);
-            mapView[startR][c + 1] = mapView[startR][c];
+        for (int c = eC; c > sC; c--) {
+            min = Math.min(min, mapView[sR][c]);
+            mapView[sR][c] = mapView[sR][c - 1];
         }
         
-        num = Math.min(num, tmp);
-        mapView[startR][startC + 1] = tmp;
+        min = Math.min(min, tmp);
+        mapView[sR][sC +1] = tmp;
         
-        return num;
+        return min;
+        
     }
 }
