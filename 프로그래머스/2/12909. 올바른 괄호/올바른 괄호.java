beb212946cbb8a0deca1ed3eff414e9183b0a57a
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-11-03 01:39:09 +0900

    [level 2] Title: 올바른 괄호, Time: 23.71 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty() || stack.pop() != '(') {
+                return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
