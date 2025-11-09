28c34b91a7210c3589321e440c347dfeb17a5d8b
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-11-10 01:37:22 +0900

    [level 3] Title: 등굣길, Time: 0.89 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 51.9 MB, 시간: 0.65 ms
+메모리: 72.4 MB, 시간: 0.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 11일 17:46:05
+2025년 11월 10일 01:37:20
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,16 +1,12 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        
         boolean[][] visited = new boolean[n + 1][m + 1];
         int[][] dp = new int[n + 1][m + 1];
-        
         for (int[] p : puddles) {
             visited[p[1]][p[0]] = true;
         }
-        
         dp[1][1] = 1;
-        
         for (int r = 1; r <= n; r++) {
             for (int c = 1; c <= m; c++) {
                 if (visited[r][c]) {
@@ -21,9 +17,7 @@ class Solution {
                 dp[r][c] += dp[r][c-1] % 1_000_000_007;
             }
         }
-        
         answer = (int) (dp[n][m]) % 1_000_000_007;
-        
         return answer;
     }
 }
