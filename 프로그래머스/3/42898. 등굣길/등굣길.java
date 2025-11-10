9a33e95338de18465e2cc2ddcf2cec59d2a73a72
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-11-10 21:06:42 +0900

    [level 3] Title: 등굣길, Time: 3.53 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,23 +1,27 @@
+import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        boolean[][] visited = new boolean[n + 1][m + 1];
-        int[][] dp = new int[n + 1][m + 1];
-        for (int[] p : puddles) {
-            visited[p[1]][p[0]] = true;
-        }
+        int[][] dp = new int[m + 1][n + 1];
         dp[1][1] = 1;
-        for (int r = 1; r <= n; r++) {
-            for (int c = 1; c <= m; c++) {
-                if (visited[r][c]) {
+        for (int r = 1; r <= m; r++) {
+            for (int c = 1; c <= n; c++) {
+                if (isPuddle(r, c, puddles)) {
                     continue;
                 }
-                
-                dp[r][c] += dp[r-1][c] % 1_000_000_007;
-                dp[r][c] += dp[r][c-1] % 1_000_000_007;
+                dp[r][c] += (dp[r-1][c] + dp[r][c-1]) % 1_000_000_007;
             }
         }
-        answer = (int) (dp[n][m]) % 1_000_000_007;
+        answer = dp[m][n] % 1_000_000_007;
         return answer;
+    }
+    
+    private boolean isPuddle(int r, int c, int[][] puddles) {
+        for (int[] puddle : puddles) {
+            if(puddle[0] == r && puddle[1] == c) {
+                return true;
+            }
+        }
+        return false;
     }
 }
