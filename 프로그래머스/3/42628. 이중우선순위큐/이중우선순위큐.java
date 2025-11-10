ace11b59d2d77c0268a143e466e5bedc11546f44
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-11-11 01:02:58 +0900

    [level 3] Title: 이중우선순위큐, Time: 94.85 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,59 +1,34 @@
 import java.util.*;
 
 class Solution {
-    
-    private static int[] result;
-    
-    private static TreeMap<Integer, Integer> map;
-    
     public int[] solution(String[] operations) {
-        
-        map = new TreeMap<>();
-        solve(operations);
-        
-        int[] answer = result;
-        return answer;
-    }
-    
-    private static void solve(String[] operations) {
-        
-        for (String oper : operations) {
-            
-            String[] operSplit = oper.split(" ");
-            String command = operSplit[0];
-            int num = Integer.parseInt(operSplit[1]);
-            
-            if (Objects.equals(command, "I")) {
+        int[] answer = {};
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (String op : operations) {
+            String[] split = op.split(" ");
+            String cmd = split[0];
+            int num = Integer.parseInt(split[1]);
+            if (Objects.equals(cmd, "I")) {
                 map.put(num, map.getOrDefault(num, 0) + 1);
                 continue;
             }
-            
             if (map.isEmpty()) {
                 continue;
             }
-            int key = 0;
-            if (Objects.equals(command, "D")) {
-                
-                if (num == 1) {
-                    key = map.lastKey();
-                }
-                if (num == -1) {
-                    key = map.firstKey();
-                }
-            }
             
-            if (map.get(key) > 1) {
-                map.replace(key, map.get(key) - 1);
-            } else {
+            int key = 0;
+            if (num == 1) {
+                key = map.lastKey();
+            }
+            if (num == -1) {
+                key = map.firstKey();
+            }
+            map.put(key, map.getOrDefault(key, 0) - 1);
+            if (map.get(key) <= 0) {
                 map.remove(key);
             }
         }
-        
-        if (map.isEmpty()) {
-            result = new int[]{0,0};
-        } else {
-            result = new int[]{map.lastKey(), map.firstKey()};
-        }
-        
+        answer = map.isEmpty() ? new int[]{0,0} : new int[]{map.lastKey(), map.firstKey()};
+        return answer;
     }
 }
