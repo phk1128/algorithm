afb74b13e16303c4e7ce57e8b79977bef975d1e8
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-11-11 00:40:49 +0900

    [level 2] Title: 더 맵게, Time: 1368.92 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 123 MB, 시간: 1450.67 ms
+메모리: 123 MB, 시간: 1368.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 23:0:4
+2025년 11월 11일 00:40:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,28 +1,22 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = -1;
-        int result = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        
-        for (int sc : scoville) {
-            queue.offer(sc);
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a1, a2) -> a1 - a2);
+        for (int s : scoville) {
+            queue.offer(s);
         }
         
         while (!queue.isEmpty()) {
             int min = queue.poll();
-            
             if (min >= K) {
-                return result;
+                return answer;
             }
-            
             if (!queue.isEmpty()) {
-                queue.offer(min + (queue.poll()*2));
-                result++;
+                queue.offer(min + queue.poll() * 2);
+                answer++;
             }
         }
-        
-        return answer;
+        return -1;
     }
 }
