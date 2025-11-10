d6f2d67ec182197a60931ebf57a7284f9d9e8570
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-11-10 22:36:43 +0900

    [level 3] Title: N으로 표현, Time: 32.03 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,41 +1,38 @@
 import java.util.*;
 
 class Solution {
-    private Set<Integer>[] results;
     public int solution(int N, int number) {
-        results = new HashSet[9];
-        for (int i = 1; i <= 8; i++) {
-            results[i] = new HashSet<>();
+        List<Set<Integer>> nums = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            nums.add(new HashSet<>());
         }
-        results[1].add(N);
-        
-        for (int i = 2; i <= 8; i++) {
-            for (int j = 1; j <= i / 2; j++) {
-                unionResult(results[j], results[i-j], results[i]);
-                unionResult(results[i-j], results[j], results[i]);
+        nums.get(1).add(N);
+        for (int i = 2; i < 9; i++) {
+            Set<Integer> num = nums.get(i); 
+            for (int j = 1; j < i; j++) {
+                Set<Integer> pre = nums.get(j);
+                Set<Integer> post = nums.get(i - j);
+                
+                for (int k : pre) {
+                    for (int l : post) {
+                        num.add(k * l);
+                        num.add(k + l);
+                        num.add(k - l);
+                        
+                        if (l != 0) {
+                            num.add(k / l);
+                        }
+                    }
+                }
             }
-            results[i].add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            num.add(Integer.parseInt(String.valueOf(N).repeat(i)));
         }
         
-        for (int i = 1; i <= 8; i++) {
-            if (results[i].contains(number)) {
+        for (int i = 1; i < 9; i++) {
+            if (nums.get(i).contains(number)) {
                 return i;
             }
         }
         return -1;
-    }
-    
-    private void unionResult(Set<Integer> preSet, Set<Integer> curSet, Set<Integer> nextSet) {
-        
-        for (int a : preSet) {
-            for (int b : curSet) {
-                nextSet.add(a * b);
-                nextSet.add(a - b);
-                nextSet.add(a + b);
-                if (b != 0) {
-                    nextSet.add(a / b);
-                }
-            }
-        }
     }
 }
