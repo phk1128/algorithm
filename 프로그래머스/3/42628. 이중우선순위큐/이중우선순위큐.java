83f1fcf6d63a4d56042a0cc550c0d846ed9e1c21
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-11-20 00:32:11 +0900

    [level 3] Title: 이중우선순위큐, Time: 94.73 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,34 +1,33 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] operations) {
         int[] answer = {};
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        PriorityQueue<Integer> minQ = new PriorityQueue<>();
+        PriorityQueue<Integer> maxQ = new PriorityQueue<>((i1, i2) -> i2 - i1);
         for (String op : operations) {
             String[] split = op.split(" ");
             String cmd = split[0];
             int num = Integer.parseInt(split[1]);
             if (Objects.equals(cmd, "I")) {
-                map.put(num, map.getOrDefault(num, 0) + 1);
+                minQ.offer(num);
+                maxQ.offer(num);
                 continue;
             }
-            if (map.isEmpty()) {
-                continue;
-            }
-            
-            int key = 0;
             if (num == 1) {
-                key = map.lastKey();
+                if (!maxQ.isEmpty()) {
+                    minQ.remove(maxQ.poll());
+                }
             }
-            if (num == -1) {
-                key = map.firstKey();
-            }
-            map.put(key, map.getOrDefault(key, 0) - 1);
-            if (map.get(key) <= 0) {
-                map.remove(key);
+            if(num == -1) {
+                if (!minQ.isEmpty()) {
+                    maxQ.remove(minQ.poll());
+                }
+                    
             }
         }
-        answer = map.isEmpty() ? new int[]{0,0} : new int[]{map.lastKey(), map.firstKey()};
+        int min = minQ.isEmpty() ? 0 : minQ.poll();
+        int max = maxQ.isEmpty() ? 0 : maxQ.poll();
+        answer = new int[]{max, min};
         return answer;
     }
 }
