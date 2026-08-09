01f1a442ac167743bbba9efe24bb2014120687c2
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2026-08-09 17:11:49 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,22 +1,29 @@
 def solution(n, computers):
-    global visited
-    answer = 0
-    visited = [False] * n
-
+    global parents
+    parents = []
     for i in range(n):
-        if (visited[i]):
-            continue
-        answer += 1
-        dfs(n, computers, i, 0)
-        
-    return answer
-
-
-def dfs(n, computers, node, depth):
-    if depth >= n:
-        return
+        parents.append(i)
+    
     for i in range(n):
-        if (visited[i] or computers[node][i] == 0):
-            continue
-        visited[i] = True
-        dfs(n, computers, i, depth + 1)
+        for j in range(n):
+            if (computers[i][j] == 1):
+                union(i, j)
+    s = set()
+    for i in range(n):
+        s.add(find(i))
+
+    return len(s)
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if (x > y):
+        parents[x] = y
+    if (x < y):
+        parents[y] = x
+
+def find(x):
+    if (parents[x] == x):
+        return x
+    return find(parents[x])
